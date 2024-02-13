271400f89ec5662ddfac378cbec61ae94e05248d
Andre <andrealbuquerque2001@gmail.com>
2024-02-13 17:50:10 -0300

    route logout

--- back-end/routes/api.php
+++ back-end/routes/api.php
@@ -17,6 +17,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->group(function () {
     Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/logout', [AuthController::class, 'logout']);
 });
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
