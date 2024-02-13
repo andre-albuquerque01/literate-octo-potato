5d4baedb961e159a1f5f5045530b21b2013071be
Andre <andrealbuquerque2001@gmail.com>
2024-02-13 14:21:06 -0300

    login finish

--- back-end/app/Http/Controllers/Auth/AuthController.php
+++ back-end/app/Http/Controllers/Auth/AuthController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\AuthLoginRequest;
 use App\Services\AuthService;
-use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
@@ -22,7 +21,7 @@ class AuthController extends Controller
         return $this->authService->login($input['email'], $input['password']);
     }
 
-    public function logout(string $id)
+    public function logout()
     {
         //
     }
--- back-end/app/Services/AuthService.php
+++ back-end/app/Services/AuthService.php
@@ -3,20 +3,26 @@
 namespace App\Services;
 
 use App\Exceptions\LoginInvalidException;
+use App\Http\Resources\AuthLoginResource;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthService
 {
+    private $request;
+    public function __construct(Request $request)
+    {
+        $this->request = $request;
+    }
+
     public function login(string $email, string $password)
     {
         $login = ['email' => $email, 'password' => $password];
 
-        if (!$token = auth()->attempt($login)) {
+        if (!Auth::attempt($login)) {
             throw new LoginInvalidException;
         }
-
-        return response()->json([
-            'access_token' => $token,
-            'token_type' => 'Bearer',
-        ]);
+        $token = $this->request->user()->createToken('user', ['*'], now()->addHours(2))->plainTextToken;
+        return new AuthLoginResource(['token' => $token]);
     }
 }
