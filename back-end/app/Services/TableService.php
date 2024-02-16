887a6455cc9eaa66fe8a7b982257901eaf0ecfbe
Andre <andrealbuquerque2001@gmail.com>
2024-02-16 10:54:36 -0300

    feat: create model, service and factory

---
+++ back-end/app/Services/TableService.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Services;
+
+class TableService
+{
+
+    public function index(){}
+    public function store(array $data){}
+    public function show(string $id){}
+    public function update(array $data, string $id){}
+    public function destroy(string $id){}
+
+}
