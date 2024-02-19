30c3ed45eefa3ef9ac789660188403e121406f78
Andre <andrealbuquerque2001@gmail.com>
2024-02-19 14:46:35 -0300

    new controller, order

---
+++ back-end/app/Http/Controllers/Api/OrderController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\OrderRequest;
+use App\Services\OrderService;
+use Illuminate\Http\Request;
+
+class OrderController extends Controller
+{
+    private $orderService;
+    public function __construct(OrderService $orderService)
+    {
+        $this->orderService = $orderService;
+    }
+
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        try {
+            return $this->orderService->index();
+        } catch (\Throwable $th) {
+            throw $th;
+        }
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(OrderRequest $request)
+    {
+        try {
+            $data = $request->validated();
+            return $this->orderService->store($data);
+        } catch (\Throwable $th) {
+            throw $th;
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        try {
+            return $this->orderService->show($id);
+        } catch (\Throwable $th) {
+            throw $th;
+        }
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(OrderRequest $request, string $id)
+    {
+        try {
+            $data = $request->validated();
+            return $this->orderService->update($data, $id);
+        } catch (\Throwable $th) {
+            throw $th;
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        try {
+            return $this->orderService->destroy($id);
+        } catch (\Throwable $th) {
+            throw $th;
+        }
+    }
+}
