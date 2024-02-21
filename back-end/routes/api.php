302f263e32918df979a3019d5702324fc04d47fb
Andre <andrealbuquerque2001@gmail.com>
2024-02-21 13:08:59 -0300

    config: configuration route

--- back-end/routes/api.php
+++ back-end/routes/api.php
@@ -23,26 +23,31 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('v1')->group(function () {
     // Login
     Route::post('/login', [AuthController::class, 'login']);
-    Route::post('/logout', [AuthController::class, 'logout']);
-    // Table
-    Route::apiResource('/table', MesaController::class);
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::post('/logout', [AuthController::class, 'logout']);
+        // Table
+        Route::apiResource('/table', MesaController::class);
+        // Menu
+        Route::apiResource('/menu', MenuController::class);
+        Route::get('/menucpf/{cpf}', [MenuController::class, 'showCPF']);
+        Route::get('/menuCodigo/{codigo}', [MenuController::class, 'showCodigo']);
+        // Order
+        Route::apiResource('/order', OrderController::class);
+    });
     // Itens
     Route::apiResource('/itens', ItensController::class);
     Route::get('/itensa', [ItensController::class, 'indexAll']);
     Route::get('/itenss/{slug}', [ItensController::class, 'showSlug']);
-    // Menu
-    Route::apiResource('/menu', MenuController::class);
-    Route::get('/menucpf/{cpf}', [MenuController::class, 'showCPF']);
-    Route::get('/menuCodigo/{codigo}', [MenuController::class, 'showCodigo']);
     // User
     Route::apiResource('/user', UserController::class);
+    Route::get('/userShow', [UserController::class, 'show']);
+    Route::put('/userUpdate', [UserController::class, 'update']);
+    Route::get('/userDelete', [UserController::class, 'destroy']);
     Route::post('/reSendEmail', [UserController::class, 'reSendEmail']);
     Route::get('/verifyEmail/{email}', [UserController::class, 'verifyEmail']);
     Route::post('/sendTokenRecover', [UserController::class, 'sendTokenRecover']);
     Route::post('/verifyTokenRecover', [UserController::class, 'verifyTokenRecover']);
     Route::put('/updatePassword/{token}', [UserController::class, 'updatePassword']);
-    // Order
-    Route::apiResource('/order', OrderController::class);
 });
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
