9e33d287d7818dd78bc095d844ae030bdb969e39
Andre <andrealbuquerque2001@gmail.com>
2024-02-22 13:44:47 -0300

    update in dashboard home

--- front-end/src/app/(dashboard)/(home)/layout.tsx
+++ front-end/src/app/(dashboard)/(home)/layout.tsx
@@ -1,7 +1,11 @@
 import { ReactNode } from 'react'
+import { Header } from '@/components/header'
 
 export default function Dashboard({ children }: { children: ReactNode }) {
   return (
-    <div className="mx-auto p-6 w-full h-screen max-w-[1600px]">{children}</div>
+    <div className="mx-auto px-5 py-2 w-full h-screen max-w-[1600px]">
+      <Header />
+      {children}
+    </div>
   )
 }
