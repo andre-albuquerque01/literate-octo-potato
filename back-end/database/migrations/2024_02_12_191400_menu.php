0a8d1b0779605391d5649037ba4a852d595bc8ec
Andre <andrealbuquerque2001@gmail.com>
2024-02-23 11:09:27 -0300

    update migration, other name went update in service

--- back-end/database/migrations/2024_02_12_191400_menu.php
+++ back-end/database/migrations/2024_02_12_191400_menu.php
@@ -22,7 +22,6 @@ return new class extends Migration
             $table->string('statusOrder', 20)->nullable();
             $table->string('methodPay');
             $table->string('value');
-            // Adicionar quem atendeu
             $table->timestamps();
         });
     }
