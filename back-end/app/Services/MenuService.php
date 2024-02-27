b335371052276186e481276e54a28982c1f8d518
Andre <andrealbuquerque2001@gmail.com>
2024-02-27 11:28:37 -0300

    improved MenuService

--- back-end/app/Services/MenuService.php
+++ back-end/app/Services/MenuService.php
@@ -30,7 +30,7 @@ class MenuService
     {
         try {
             $data['codigo'] = strtoupper(Str::random(10));
-            $data['idUser'] = 1;
+            $data['idUser'] = $this->user->idUser;
             Menu::create($data);
             return response()->json(['message' => 'sucess'], 200);
         } catch (\Throwable $th) {
