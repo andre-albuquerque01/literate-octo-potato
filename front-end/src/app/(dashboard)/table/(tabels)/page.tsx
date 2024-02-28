14a1b8f76d7dfb3a7bc0c0a395e39ce8a171f461
Andre <andrealbuquerque2001@gmail.com>
2024-02-28 13:31:01 -0300

    improvement page

--- front-end/src/app/(dashboard)/table/(tabels)/page.tsx
+++ front-end/src/app/(dashboard)/table/(tabels)/page.tsx
@@ -4,12 +4,12 @@ import Link from 'next/link'
 
 export default function Tables() {
   return (
-    <div className="max-md:w-[80%] max-md:mx-auto">
+    <div className="max-md:w-[80%] max-md:mx-auto md:mt-4">
       <Link href="" className="flex items-center py-4 md:hidden">
         <ArrowLeft className="w-5 h-5" /> Voltar
       </Link>
       <h1 className="text-2xl">Mesas</h1>
-      <div className="space-y-5 mt-5">
+      <div className="max-md:space-y-5 mt-5 md:flex md:gap-5 md:flex-wrap md:items-center md:w-full">
         <TableStatus numberTable={1} status="vazia" />
         <TableStatus numberTable={2} status="ocupada" />
         <TableStatus numberTable={3} status="vazia" />
