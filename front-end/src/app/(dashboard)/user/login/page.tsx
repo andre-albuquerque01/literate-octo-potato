160d08bd864f6c21d578cf9c511853d7b685b066
Andre <andrealbuquerque2001@gmail.com>
2024-02-29 17:32:43 -0300

    request api login

--- front-end/src/app/(dashboard)/user/login/page.tsx
+++ front-end/src/app/(dashboard)/user/login/page.tsx
@@ -1,19 +1,48 @@
+'use client'
 import { BtnForm } from '@/components/btnForm'
+import Api from '@/data/api'
 import { ArrowLeft } from 'lucide-react'
 import Link from 'next/link'
+import { FormEvent, useState } from 'react'
+
+async function postLogin(body: object) {
+  try {
+    const response = await Api('/user/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(body),
+    })
+    if (response.ok) return response.json()
+  } catch (error) {
+    console.error(error)
+  }
+}
 
 export default function Login() {
+  const [error, setError] = useState<string>('')
+
+  async function handleSubmit(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault()
+
+    const formData = new FormData(e.currentTarget)
+    const data = Object.fromEntries(formData)
+    const req = await postLogin(data)
+    setError(req)
+  }
+
   return (
-    <div className="flex flex-col mx-auto justify-center h-screen w-full items-center">
+    <div className="flex flex-col mx-auto justify-center md:h-[80%] max-md:h-[100%] w-full items-center">
       <Link
         href="/"
-        className="flex items-center gap-1 text-sm mb-3 w-96 max-md:w-80"
+        className="md:hidden flex items-center gap-1 text-sm mb-10 w-96 max-md:w-80"
       >
         <ArrowLeft className="w-5 h-5" />
         Voltar
       </Link>
       <p className="text-xl w-96 max-md:mb-0 max-md:w-80">Login</p>
-      <form className="w-96 max-md:w-80">
+      <form className="w-96 max-md:w-80" onSubmit={handleSubmit}>
         <div className="flex flex-col mt-3">
           <label htmlFor="email">
             E-mail: <span className="text-red-600">*</span>{' '}
@@ -26,6 +55,7 @@ export default function Login() {
             required
           />
         </div>
+        <span className="text-red-600 text-xs">{error}</span>
         <div className="flex flex-col mt-3">
           <label htmlFor="password">
             Senha: <span className="text-red-600">*</span>{' '}
