110f61485aa6e492828f420acded4f621acdce74
Andre <andrealbuquerque2001@gmail.com>
2024-02-29 17:31:36 -0300

    new api config, localhost 3000

---
+++ front-end/src/data/api.ts
@@ -0,0 +1,7 @@
+export default function Api(path: string, init?: RequestInit) {
+  const baseUrl = 'http://localhost:3000'
+  const apiPrefix = '/api'
+  const url = new URL(apiPrefix.concat(path), baseUrl)
+
+  return fetch(url, init)
+}
