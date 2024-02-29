60cbd68e889d81128feca8327cc72e4d5a5939ef
Andre <andrealbuquerque2001@gmail.com>
2024-02-29 17:32:26 -0300

    insert user

--- front-end/src/app/(dashboard)/user/insert/page.tsx
+++ front-end/src/app/(dashboard)/user/insert/page.tsx
@@ -1,19 +1,78 @@
+'use client'
 import { BtnForm } from '@/components/btnForm'
+import Api from '@/data/api'
 import { ArrowLeft } from 'lucide-react'
 import Link from 'next/link'
+import { FormEvent, useState } from 'react'
+
+async function postInsert(body: object) {
+  try {
+    const response = await Api('/user/insert', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(body),
+    })
+    if (response.ok) return response.json()
+  } catch (error) {
+    console.error(error)
+  }
+}
 
 export default function InsertUser() {
+  const [returnError, setReturnError] = useState<string>('')
+
+  const hasNumber = /\d/
+  const hasUpperCase = /[A-Z]/
+  const hasLowerCase = /[a-z]/
+  const hasSymbol = /[!@#$%^&*(),.?":{}|<>_=+]/
+
+  const err = [
+    'Senha precisa ter pelo menos um número.',
+    'Senha precisa ter pelo menos uma letra maiúscula.',
+    'Senha precisa ter pelo menos uma letra minúscula.',
+    'Senha precisa ter pelo menos um símbolo.',
+    'Senhas não correspondem',
+  ]
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  function validatePassword(password: any) {
+    if (!hasNumber.test(password)) return err[0]
+    if (!hasUpperCase.test(password)) return err[1]
+    if (!hasLowerCase.test(password)) return err[2]
+    if (!hasSymbol.test(password)) return err[3]
+    return ''
+  }
+
+  async function handleSubmit(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault()
+
+    const formData = new FormData(e.currentTarget)
+    const data = Object.fromEntries(formData)
+    setReturnError(validatePassword(data.password))
+    if (
+      data.password === data.password_confirmation &&
+      validatePassword(data.password) === ''
+    ) {
+      console.log('foi aqui')
+      const req = await postInsert(data)
+      setReturnError(req)
+      return ''
+    }
+    return ''
+  }
   return (
-    <div className="flex flex-col mx-auto justify-center h-screen w-full items-center">
+    <div className="flex flex-col mx-auto justify-center h-[90%] w-full items-center">
       <Link
         href="/"
-        className="flex items-center gap-1 text-sm mb-3 w-96 max-md:mt-24 max-md:w-80"
+        className="md:hidden flex items-center gap-1 text-sm mb-3 w-96 max-md:mt-24 max-md:w-80"
       >
         <ArrowLeft className="w-5 h-5" />
         Voltar
       </Link>
       <p className="text-xl mb-1 w-96 max-md:mb-0 max-md:w-80">Seu cadastro</p>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="flex flex-col mt-3 max-md:mt-3">
           <label htmlFor="firstName">
             Primeiro nome: <span className="text-red-600">*</span>{' '}
@@ -74,6 +133,9 @@ export default function InsertUser() {
             required
           />
         </div>
+        {returnError === 'The cpf has already been taken.' && (
+          <span className="text-xs text-red-600">CPF já cadastrado</span>
+        )}
         <div className="flex flex-col mt-3">
           <label htmlFor="email">
             E-mail: <span className="text-red-600">*</span>{' '}
@@ -86,6 +148,9 @@ export default function InsertUser() {
             required
           />
         </div>
+        {returnError === 'The email has already been taken.' && (
+          <span className="text-xs text-red-600">E-mail já cadastrado</span>
+        )}
         <div className="flex flex-col mt-3">
           <label htmlFor="password">
             Senha: <span className="text-red-600">*</span>{' '}
@@ -98,6 +163,14 @@ export default function InsertUser() {
             required
           />
         </div>
+        {err.map(
+          (erro, key) =>
+            returnError === erro && (
+              <span key={key} className="text-xs text-red-600">
+                {returnError}
+              </span>
+            ),
+        )}
         <div className="flex flex-col mt-3">
           <label htmlFor="password_confirmation">
             Repetir senha: <span className="text-red-600">*</span>{' '}
