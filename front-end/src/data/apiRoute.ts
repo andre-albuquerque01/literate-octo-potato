d4353a974e84457ee6ed1105fc7c572f0a787d86
Andre <andrealbuquerque2001@gmail.com>
2024-02-29 17:31:23 -0300

    new config route

---
+++ front-end/src/data/apiRoute.ts
@@ -0,0 +1,7 @@
+export default function ApiRoute(path: string, init?: RequestInit) {
+  const baseUrl = 'http://localhost'
+  const apiPrefix = '/api/v1'
+  const url = new URL(apiPrefix.concat(path), baseUrl)
+
+  return fetch(url, init)
+}
