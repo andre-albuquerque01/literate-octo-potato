e8ef94be46ce7082bccf14b1b7b668eef6df0ecd
Andre <andrealbuquerque2001@gmail.com>
2024-03-04 15:35:27 -0300

    new route slug

---
+++ front-end/src/app/api/itens/slug/[slug]/route.ts
@@ -0,0 +1,23 @@
+import ApiRoute from '@/data/apiRoute'
+import { NextResponse } from 'next/server'
+
+export default async function GET({ params }: { params: { slug: string } }) {
+  try {
+    const slug = params.slug
+
+    const response = await ApiRoute(`/itenss/${slug}`, {
+      next: {
+        revalidate: 60,
+      },
+    })
+
+    const data = await response.json()
+    console.log(data)
+
+    return NextResponse.json({ data })
+  } catch (error) {
+    return new Response(JSON.stringify('error'), {
+      status: 401,
+    })
+  }
+}
