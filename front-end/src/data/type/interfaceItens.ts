b0234dc1e1e464da02b043d109e04ede3274e92b
Andre <andrealbuquerque2001@gmail.com>
2024-03-05 11:16:58 -0300

    new interface

---
+++ front-end/src/data/type/interfaceItens.ts
@@ -0,0 +1,12 @@
+export interface InterfaceItens {
+  title: string
+  desc: string
+  value: number
+  statusIten: boolean
+  qtdIten: number
+  slug: string
+  urlImage: string
+  waitTime: string
+  category: string
+  position: string
+}
