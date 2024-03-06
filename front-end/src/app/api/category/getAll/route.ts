caa9fea7fe1676057ecb6f8b82c33c578e0cc471
Andre <andrealbuquerque2001@gmail.com>
2024-03-06 16:05:31 -0300

    improvement route

---
+++ front-end/src/app/api/category/getAll/route.ts
@@ -0,0 +1,24 @@
+import ApiRoute from '@/data/apiRoute'
+import { NextResponse } from 'next/server'
+
+export async function GET() {
+  try {
+    const response = await ApiRoute(`/category`, {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      next: {
+        revalidate: 60,
+      },
+    })
+
+    const data = await response.json()
+
+    return NextResponse.json({ data })
+  } catch (error) {
+    console.error(error)
+    throw error
+  }
+}
