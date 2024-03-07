3af5f4948cd15219cce1981820da88e17d4a5cce
Andre <andrealbuquerque2001@gmail.com>
2024-03-07 15:06:36 -0300

    new route

---
+++ front-end/src/app/api/itens/[id]/route.ts
@@ -0,0 +1,28 @@
+import ApiRoute from '@/data/apiRoute'
+import { NextResponse } from 'next/server'
+
+export async function GET(_: Request, { params }: { params: { id: number } }) {
+  try {
+    const id = params.id
+
+    const response = await ApiRoute(`/itens/${id}`, {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      cache: 'no-cache',
+      // next: {
+      //   revalidate: 60,
+      // },
+    })
+
+    const data = await response.json()
+
+    return NextResponse.json({ data })
+  } catch (error) {
+    return new Response(JSON.stringify(error), {
+      status: 401,
+    })
+  }
+}
