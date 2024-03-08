a560a0c458f68037c10bb7d384eac9594a1a3143
Andre <andrealbuquerque2001@gmail.com>
2024-03-08 13:05:04 -0300

    new route

---
+++ front-end/src/app/api/itens/rate/insert/route.ts
@@ -0,0 +1,28 @@
+import ApiRoute from '@/data/apiRoute'
+import { cookies } from 'next/headers'
+
+export async function POST(request: Request) {
+  try {
+    const requestBody = await request.json()
+    const cookiesStore = cookies()
+    const token = cookiesStore.get('token')
+
+    const response = await ApiRoute('/rate', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token?.value}`,
+      },
+      body: JSON.stringify(requestBody),
+    })
+
+    const data = await response.json()
+
+    return Response.json({ data })
+  } catch (error) {
+    return new Response(JSON.stringify('error'), {
+      status: 401,
+    })
+  }
+}
