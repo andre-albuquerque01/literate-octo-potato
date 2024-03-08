7086e1fcca71b22acb0b585f997ee662a2981da2
Andre <andrealbuquerque2001@gmail.com>
2024-03-08 12:20:14 -0300

    update request in page

--- front-end/src/app/(dashboard)/category/insert/page.tsx
+++ front-end/src/app/(dashboard)/category/insert/page.tsx
@@ -1,13 +1,41 @@
+'use client'
 import { BtnForm } from '@/components/btnForm'
+import Api from '@/data/api'
 import { ArrowLeft } from 'lucide-react'
 import Link from 'next/link'
+import { FormEvent } from 'react'
+
+async function postInsert(body: object) {
+  try {
+    const request = await Api('category/insert', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application-json',
+      },
+      body: JSON.stringify(body),
+    })
+    const reqJson = await request.json()
+    return reqJson
+  } catch (error) {
+    console.error(error)
+    throw error
+  }
+}
 
 export default function InsertCategory() {
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    const formData = new FormData(e.currentTarget)
+    const data = Object.fromEntries(formData)
+    const req = await postInsert(data)
+    console.log(req)
+  }
+
   return (
-    <div className="flex flex-col mx-auto justify-center h-screen w-full items-center">
+    <div className="flex flex-col mx-auto justify-center h-[90%] w-full items-center">
       <Link
         href="/"
-        className="flex items-center gap-1 text-sm mb-3 w-96 max-md:mt-24 max-md:w-80"
+        className="md:hidden flex items-center gap-1 text-sm mb-3 w-96 max-md:mt-24 max-md:w-80"
       >
         <ArrowLeft className="w-5 h-5" />
         Voltar
@@ -15,7 +43,7 @@ export default function InsertCategory() {
       <p className="text-xl mb-1 w-96 max-md:mb-0 max-md:w-80">
         Cadastro de categoria
       </p>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="flex flex-col mt-3 max-md:mt-3">
           <label htmlFor="typeCategory">
             Nome da categoria: <span className="text-red-600">*</span>{' '}
@@ -30,8 +58,7 @@ export default function InsertCategory() {
         </div>
         <div className="flex flex-col mt-3 max-md:mt-3">
           <label htmlFor="urlImageCategory">
-            Imagem de fundo da categoria:{' '}
-            <span className="text-red-600">*</span>{' '}
+            Imagem de fundo da categoria:
           </label>
           <input
             type="text"
