d4a2aa3ea9dfb79c6966cdd8dc407018133e9c22
Andre <andrealbuquerque2001@gmail.com>
2024-03-12 10:27:09 -0300

    update route

---
+++ front-end/src/app/api/category/update/[id]/route.ts
@@ -0,0 +1,29 @@
+import ApiRoute from '@/data/apiRoute'
+import { NextResponse } from 'next/server'
+
+export async function PUT(
+  request: Request,
+  { params }: { params: { id: number } },
+) {
+  try {
+    const id = params.id
+    const reqBody = await request.json()
+
+    const response = await ApiRoute(`/category/${id}`, {
+      method: 'PUT',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(reqBody),
+    })
+
+    const data = await response.json()
+
+    return NextResponse.json({ data })
+  } catch (error) {
+    return new Response(JSON.stringify(error), {
+      status: 401,
+    })
+  }
+}
