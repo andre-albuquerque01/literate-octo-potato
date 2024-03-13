95712cfb2891cdcca173fc3a22960d268eb98646
Andre <andrealbuquerque2001@gmail.com>
2024-03-13 11:10:15 -0300

    feat: route update user

---
+++ front-end/src/app/api/user/update/route.ts
@@ -0,0 +1,30 @@
+import ApiRoute from '@/data/apiRoute'
+import { cookies } from 'next/headers'
+import { NextResponse } from 'next/server'
+
+export async function PUT(request: Request) {
+  try {
+    const reqBody = await request.json()
+
+    const cookiesStore = cookies()
+    const token = cookiesStore.get('token')
+
+    const response = await ApiRoute('/userUpdate', {
+      method: 'PUT',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token?.value}`,
+      },
+      body: JSON.stringify(reqBody),
+    })
+
+    const data = await response.json()
+
+    return NextResponse.json({ data })
+  } catch (error) {
+    return new Response(JSON.stringify(error), {
+      status: 401,
+    })
+  }
+}
