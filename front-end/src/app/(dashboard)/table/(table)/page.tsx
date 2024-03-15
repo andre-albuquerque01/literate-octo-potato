58499189fe486c1408252bbb08307ca439b8493d
Andre <andrealbuquerque2001@gmail.com>
2024-03-15 11:16:02 -0300

    feat: update in page

--- front-end/src/app/(dashboard)/table/(table)/page.tsx
+++ front-end/src/app/(dashboard)/table/(table)/page.tsx
@@ -1,8 +1,26 @@
 import { TableStatus } from '@/components/Table-status'
+import Api from '@/data/api'
+import { TableInterface } from '@/data/type/table'
 import { ArrowLeft } from 'lucide-react'
 import Link from 'next/link'
 
-export default function Tables() {
+async function getAll(): Promise<TableInterface[]> {
+  try {
+    const request = await Api('/table/getAll', {
+      method: 'GET',
+      cache: 'no-cache',
+    })
+    const reqBody = await request.json()
+    return reqBody.data.data
+  } catch (error) {
+    console.error(error)
+    throw error
+  }
+}
+
+export default async function Tables() {
+  const data = await getAll()
+
   return (
     <div className="max-md:w-[80%] max-md:mx-auto md:mt-4">
       <Link href="" className="flex items-center py-4 md:hidden">
@@ -10,11 +28,15 @@ export default function Tables() {
       </Link>
       <h1 className="text-2xl">Mesas</h1>
       <div className="max-md:space-y-5 mt-5 md:flex md:gap-5 md:flex-wrap md:items-center md:w-full">
-        <TableStatus numberTable={1} status="vazia" />
-        <TableStatus numberTable={2} status="ocupada" />
-        <TableStatus numberTable={3} status="vazia" />
-        <TableStatus numberTable={4} status="vazia" />
-        <TableStatus numberTable={5} status="ocupada" />
+        {data.map((table, index) => (
+          <TableStatus
+            key={index}
+            numberTable={table.numberMesa}
+            lotacao={table.lotacao}
+            statusMesa={table.statusMesa}
+            idMesa={table.idMesa}
+          />
+        ))}
       </div>
     </div>
   )
--- front-end/src/app/(dashboard)/table/update/[id]/page.tsx
+++ front-end/src/app/(dashboard)/table/update/[id]/page.tsx
@@ -78,7 +78,7 @@ export default function InsertTable({ params }: { params: { id: number } }) {
   return (
     <div className="flex flex-col mx-auto justify-center h-[80%] w-full items-center">
       <p className="text-xl mb-1 w-96 max-md:mb-0 max-md:w-80">
-        Cadastro da mesa
+        Alterar mesa <span className="font-medium">{data.numberMesa}</span>
       </p>
       <form onSubmit={handleSubmit}>
         <div className="flex flex-col mt-3 max-md:mt-3">
