986c4697164ad449fe2bf2519d449d6ee541498d
Andre <andrealbuquerque2001@gmail.com>
2024-03-15 11:19:58 -0300

    feat: new page, list table, and update component

---
+++ front-end/src/app/(dashboard)/table/list/page.tsx
@@ -0,0 +1,77 @@
+import Api from '@/data/api'
+import { TableInterface } from '@/data/type/table'
+import { ArrowLeft, Edit, PlusCircle, Trash2 } from 'lucide-react'
+import Link from 'next/link'
+
+async function getAll(): Promise<TableInterface[]> {
+  try {
+    const request = await Api('/table/getAll', {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      next: {
+        revalidate: 1,
+      },
+    })
+    const reqJson = await request.json()
+    return reqJson.data.data
+  } catch (error) {
+    console.error(error)
+    throw error
+  }
+}
+
+export default async function GetAll() {
+  const data = await getAll()
+
+  return (
+    <div className="flex flex-col min-h-[90%] w-full px-3">
+      <Link
+        href="/"
+        className="md:hidden flex items-center gap-1 text-sm mb-3 w-96 max-md:mt-3 max-md:w-80"
+      >
+        <ArrowLeft className="w-5 h-5" />
+        Voltar
+      </Link>
+      <Link
+        href="/table/insert"
+        className="flex items-center gap-2 w-[170px] py-2 text-sm max-md:mb-0 max-md:w-80 mt-3 border-b-2 border-transparent hover:border-red-600 transition duration-500"
+      >
+        <PlusCircle className="w-5 h-5" />
+        Adicionar mesa
+      </Link>
+      <p className="text-xl mb-1 w-96 max-md:mb-0 max-md:w-80 mt-5">
+        Lista das mesas cadastradas
+      </p>
+      <div className="space-y-5 mt-4">
+        {data.map((table, key) => (
+          <div className="" key={key}>
+            <div className="flex mx-auto justify-between">
+              <span className="max-w-96">Número: {table.numberMesa}</span>
+              <span className="max-md:hidden truncate w-96 text-center">
+                Capacidade: {table.lotacao}
+              </span>
+              <div className="flex items-center gap-3">
+                <Link
+                  href={`/table/update/${table.idMesa}`}
+                  title={`Editar o item, ${table.numberMesa}`}
+                >
+                  <Edit className="w-5 h-5" />
+                </Link>
+                <Link
+                  href={`/table/update/${table.idMesa}`}
+                  title={`Excluir o item, ${table.numberMesa}`}
+                >
+                  <Trash2 className="w-5 h-5" />
+                </Link>
+              </div>
+            </div>
+            <div className="w-full h-[1px] bg-zinc-600"></div>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
--- front-end/src/components/Table-status.tsx
+++ front-end/src/components/Table-status.tsx
@@ -1,21 +1,70 @@
+'use client'
+import Api from '@/data/api'
 import { X, CheckCircle } from 'lucide-react'
 
 interface Table {
-  numberTable: number
-  status: string
+  numberTable: string
+  lotacao: string
+  statusMesa: number
+  idMesa: number
+}
+
+async function putStatusTable(body: object, id: number) {
+  try {
+    const request = await Api(`/table/update/${id}`, {
+      method: 'PUT',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(body),
+    })
+    const reqBody = await request.json()
+
+    return reqBody.data
+  } catch (error) {
+    console.error(error)
+    throw error
+  }
 }
 
 export const TableStatus = (props: Table) => {
+  const handleBlock = async (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+  ) => {
+    e.preventDefault()
+    const table = await putStatusTable({ statusMesa: 1 }, props.idMesa)
+    if (table.message === 'sucess') {
+      alert('Mesa torna-se ocupada!')
+      window.location.reload()
+    }
+  }
+
+  const handleOpen = async (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+  ) => {
+    e.preventDefault()
+    const table = await putStatusTable({ statusMesa: 0 }, props.idMesa)
+    if (table.message === 'sucess') {
+      alert('Mesa torna-se vazia!')
+      window.location.reload()
+    }
+  }
+
   return (
     <div className="md:w-[20%] flex justify-between items-center border border-zinc-400 p-4 shadow-md">
       <div className="">
         <p>Mesa {props.numberTable}</p>
-        <p className="">{props.status}</p>
+        <p className="">{props.statusMesa === 0 ? 'Vazia' : 'Ocupada'}</p>
       </div>
-      {props.status === 'vazia' ? (
-        <CheckCircle className="w-5 h-5  rounded-full" />
+      {props.statusMesa === 0 ? (
+        <button onClick={handleBlock}>
+          <CheckCircle className="w-5 h-5  rounded-full" />
+        </button>
       ) : (
-        <X className="w-5 h-5 bg-black text-white rounded-full" />
+        <button onClick={handleOpen}>
+          <X className="w-5 h-5 bg-black text-white rounded-full" />
+        </button>
       )}
     </div>
   )
