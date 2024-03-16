4739cb8a6bffde68bb8ab9335b2ff16842ec32db
Andre <andrealbuquerque2001@gmail.com>
2024-03-16 09:52:37 -0300

    feat: improvement in page

--- front-end/src/app/(dashboard)/table/(table)/page.tsx
+++ front-end/src/app/(dashboard)/table/(table)/page.tsx
@@ -1,26 +1,8 @@
 import { TableStatus } from '@/components/Table-status'
-import Api from '@/data/api'
-import { TableInterface } from '@/data/type/table'
 import { ArrowLeft } from 'lucide-react'
 import Link from 'next/link'
 
-async function getAll(): Promise<TableInterface[]> {
-  try {
-    const request = await Api('/table/getAll', {
-      method: 'GET',
-      cache: 'no-cache',
-    })
-    const reqBody = await request.json()
-    return reqBody.data.data
-  } catch (error) {
-    console.error(error)
-    throw error
-  }
-}
-
 export default async function Tables() {
-  const data = await getAll()
-
   return (
     <div className="max-md:w-[80%] max-md:mx-auto md:mt-4">
       <Link href="" className="flex items-center py-4 md:hidden">
@@ -28,15 +10,7 @@ export default async function Tables() {
       </Link>
       <h1 className="text-2xl">Mesas</h1>
       <div className="max-md:space-y-5 mt-5 md:flex md:gap-5 md:flex-wrap md:items-center md:w-full">
-        {data.map((table, index) => (
-          <TableStatus
-            key={index}
-            numberTable={table.numberMesa}
-            lotacao={table.lotacao}
-            statusMesa={table.statusMesa}
-            idMesa={table.idMesa}
-          />
-        ))}
+        <TableStatus />
       </div>
     </div>
   )
