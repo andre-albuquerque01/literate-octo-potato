67a1ed382e3c7472337ca7c66282e86a7a67f3de
Andre <andrealbuquerque2001@gmail.com>
2024-03-16 09:53:07 -0300

    feat: improvoment in componetn, request api table

--- front-end/src/components/Table-status.tsx
+++ front-end/src/components/Table-status.tsx
@@ -1,12 +1,21 @@
 'use client'
 import Api from '@/data/api'
+import { TableInterface } from '@/data/type/table'
 import { X, CheckCircle } from 'lucide-react'
+import { useEffect, useState } from 'react'
 
-interface Table {
-  numberTable: string
-  lotacao: string
-  statusMesa: number
-  idMesa: number
+async function getAll(): Promise<TableInterface[]> {
+  try {
+    const request = await Api('/table/getAll', {
+      method: 'GET',
+      cache: 'no-cache',
+    })
+    const reqBody = await request.json()
+    return reqBody.data.data
+  } catch (error) {
+    console.error(error)
+    throw error
+  }
 }
 
 async function putStatusTable(body: object, id: number) {
@@ -28,44 +37,75 @@ async function putStatusTable(body: object, id: number) {
   }
 }
 
-export const TableStatus = (props: Table) => {
+export const TableStatus = () => {
+  const [data, setData] = useState<TableInterface[]>([])
+  const [status, setStatus] = useState<boolean>(false)
+
+  useEffect(() => {
+    const getAl = async () => {
+      const get = await getAll()
+      setData(get)
+    }
+    getAl()
+  }, [])
+
+  useEffect(() => {
+    const getAl = async () => {
+      const getA = await getAll()
+      setData(getA)
+    }
+    if (status) {
+      getAl()
+      setStatus(false)
+    }
+  }, [status])
+
   const handleBlock = async (
+    id: number,
     e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
   ) => {
     e.preventDefault()
-    const table = await putStatusTable({ statusMesa: 1 }, props.idMesa)
+    const table = await putStatusTable({ statusMesa: 1 }, id)
     if (table.message === 'sucess') {
       alert('Mesa torna-se ocupada!')
-      window.location.reload()
+      setStatus(true)
     }
   }
 
   const handleOpen = async (
+    id: number,
     e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
   ) => {
     e.preventDefault()
-    const table = await putStatusTable({ statusMesa: 0 }, props.idMesa)
+    const table = await putStatusTable({ statusMesa: 0 }, id)
     if (table.message === 'sucess') {
       alert('Mesa torna-se vazia!')
-      window.location.reload()
+      setStatus(true)
     }
   }
 
   return (
-    <div className="md:w-[20%] flex justify-between items-center border border-zinc-400 p-4 shadow-md">
-      <div className="">
-        <p>Mesa {props.numberTable}</p>
-        <p className="">{props.statusMesa === 0 ? 'Vazia' : 'Ocupada'}</p>
-      </div>
-      {props.statusMesa === 0 ? (
-        <button onClick={handleBlock}>
-          <CheckCircle className="w-5 h-5  rounded-full" />
-        </button>
-      ) : (
-        <button onClick={handleOpen}>
-          <X className="w-5 h-5 bg-black text-white rounded-full" />
-        </button>
-      )}
-    </div>
+    <>
+      {data.map((table, index) => (
+        <div
+          key={index}
+          className="md:w-[20%] flex justify-between items-center border border-zinc-400 p-4 shadow-md"
+        >
+          <div className="">
+            <p>Mesa {table.numberMesa}</p>
+            <p className="">{table.statusMesa === 0 ? 'Vazia' : 'Ocupada'}</p>
+          </div>
+          {table.statusMesa === 0 ? (
+            <button onClick={(e) => handleBlock(table.idMesa, e)}>
+              <CheckCircle className="w-5 h-5  rounded-full" />
+            </button>
+          ) : (
+            <button onClick={(e) => handleOpen(table.idMesa, e)}>
+              <X className="w-5 h-5 bg-black text-white rounded-full" />
+            </button>
+          )}
+        </div>
+      ))}
+    </>
   )
 }
