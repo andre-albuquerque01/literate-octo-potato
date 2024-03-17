e764ea60344ecb55a9baa83d51bf28de77b77af5
Andre <andrealbuquerque2001@gmail.com>
2024-03-17 17:45:22 -0300

    feat: working function

--- front-end/src/components/btnLike.tsx
+++ front-end/src/components/btnLike.tsx
@@ -1,5 +1,6 @@
 import Api from '@/data/api'
 import { ThumbsDown, ThumbsUp } from 'lucide-react'
+import { useEffect, useState } from 'react'
 
 async function Like(body: object) {
   try {
@@ -7,6 +8,8 @@ async function Like(body: object) {
       method: 'POST',
       body: JSON.stringify(body),
     })
+    const reqBody = await request.json()
+    return reqBody
   } catch (error) {
     console.error(error)
     throw error
@@ -18,17 +21,50 @@ async function Dislike(body: object) {
     const request = await Api('', {
       method: 'DELETE',
     })
+    const reqBody = await request.json()
+    return reqBody
   } catch (error) {
     console.error(error)
     throw error
   }
 }
 
+async function VirifyLike(id: number) {
+  try {
+    const request = await Api(`${id}`, {
+      method: 'GET',
+    })
+    const reqBody = await request.json()
+    return reqBody
+  } catch (error) {
+    console.error(error)
+    throw error
+  }
+}
 
+type ProspLike = {
+  id: number
+}
 
-export const BtnLike = () => {
-  const hanldeLike = async () => {}
-  const hanldeDisLike = async () => {}
+export const BtnLike = (props: ProspLike) => {
+  const [data, setData] = useState()
+
+  useEffect(() => {
+    const handleData = async () => {
+      const dt = await VirifyLike(props.id)
+      setData(dt)
+    }
+    handleData()
+  }, [])
+
+  const hanldeLike = async () => {
+    const like = await Like({ idIten: props.id })
+    alert(like)
+  }
+  const hanldeDisLike = async () => {
+    const like = await Dislike({ idIten: props.id })
+    alert(like)
+  }
 
   return (
     <div className="space-y-2">
