9f68d4dfd6cf8638a093b2e48b7dfe257b7d560b
Andre <andrealbuquerque2001@gmail.com>
2024-03-18 19:31:02 -0300

    feat: new function in rate service

--- back-end/app/Services/RateService.php
+++ back-end/app/Services/RateService.php
@@ -23,10 +23,29 @@ class RateService
             throw $th;
         }
     }
+
+    public function showLikeUser(string $id)
+    {
+        try {
+            $idUser = $this->user->idUser;
+            $rate = Rate::where('idUser', $idUser)->where('idItens', $id)->get();
+            if ($rate->isNotEmpty())
+                return response()->json(['data' => 'true']);
+            return response()->json(['data' => 'false']);
+        } catch (\Throwable $th) {
+            throw $th;
+        }
+    }
     public function store(array $data)
     {
         try {
             $data['idUser'] = $this->user->idUser;
+            $exist = Rate::where('idUser', $data['idUser'])->where('idItens', $data['idItens'])->first();
+            
+            if ($exist) {
+                return response()->json(['message' => 'Já avaliou'], 400);
+            }
+
             Rate::create($data);
             return response()->json(['message' => 'sucess'], 200);
         } catch (\Throwable $th) {
@@ -36,8 +55,8 @@ class RateService
     public function destroy(string $id)
     {
         try {
-            Rate::findOrFail($id)->delete();
-            return response()->json(['message' => 'sucess'], 200);            
+            Rate::where('idItens', $id)->delete();
+            return response()->json(['message' => 'sucess'], 200);
         } catch (\Throwable $th) {
             throw $th;
         }
