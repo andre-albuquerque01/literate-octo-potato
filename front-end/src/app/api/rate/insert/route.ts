8c19ba255fcf3c740ce37c5bf5dcd4ec6ef77e1e
Andre <andrealbuquerque2001@gmail.com>
2024-03-18 19:29:41 -0300

    feat: new route insert rate

---
+++ front-end/src/app/api/rate/insert/route.ts
@@ -0,0 +1,29 @@
+import ApiRoute from '@/data/apiRoute'
+import { cookies } from 'next/headers'
+
+export async function POST(request: Request) {
+  try {
+    const requestBody = await request.json()
+    const cookiesStore = cookies()
+    const token = cookiesStore.get('token')
+
+    const response = await ApiRoute('/rate', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token?.value}`,
+      },
+      body: JSON.stringify(requestBody),
+    })
+
+    const data = await response.json()
+    console.log(data)
+
+    return Response.json({ data })
+  } catch (error) {
+    return new Response(JSON.stringify('error'), {
+      status: 401,
+    })
+  }
+}
