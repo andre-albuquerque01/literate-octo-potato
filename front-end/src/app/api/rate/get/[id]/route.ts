e7b87160168b7a352cbf120e0f37c128704c0a89
Andre <andrealbuquerque2001@gmail.com>
2024-03-18 19:30:04 -0300

    feat: new route getUser rate

---
+++ front-end/src/app/api/rate/get/[id]/route.ts
@@ -0,0 +1,25 @@
+import ApiRoute from '@/data/apiRoute'
+import { NextResponse } from 'next/server'
+
+export async function GET(_: Request, { params }: { params: { id: number } }) {
+  try {
+    const id = params.id
+
+    const response = await ApiRoute(`/rateU/${id}`, {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      cache: 'no-cache',
+    })
+
+    const data = await response.json()
+
+    return NextResponse.json({ data })
+  } catch (error) {
+    return new Response(JSON.stringify(error), {
+      status: 401,
+    })
+  }
+}
