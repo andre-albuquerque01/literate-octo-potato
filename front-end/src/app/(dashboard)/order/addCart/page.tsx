5d73ede5cf3d2a46af4b60ebb2ca38e09d50f521
Andre <andrealbuquerque2001@gmail.com>
2024-03-20 12:13:18 -0300

    feat: request api

--- front-end/src/app/(dashboard)/order/addCart/page.tsx
+++ front-end/src/app/(dashboard)/order/addCart/page.tsx
@@ -1,9 +1,29 @@
+import { AddCartComanda } from '@/components/addCart'
 import { BtnForm } from '@/components/btnForm'
-import { ArrowLeft, Plus, Search } from 'lucide-react'
+import Api from '@/data/api'
+import { InterfaceItens } from '@/data/type/interfaceItens'
+import { ArrowLeft, Search } from 'lucide-react'
 import Image from 'next/image'
 import Link from 'next/link'
 
-export default function AddCart() {
+async function getItens(): Promise<InterfaceItens[]> {
+  try {
+    const request = await Api('/itens/home', {
+      next: {
+        revalidate: 30,
+      },
+    })
+    const reqBody = await request.json()
+    return reqBody.data.data
+  } catch (error) {
+    console.error(error)
+    throw error
+  }
+}
+
+export default async function AddCart() {
+  const data = await getItens()
+
   return (
     <div className="max-md:w-[360px] mx-auto mt-5 space-y-5">
       <form className="flex items-center gap-3 w-full rounded-xl px-5 py-3 border border-slate-400 md:mt-7 ">
@@ -20,25 +40,35 @@ export default function AddCart() {
         <ArrowLeft className="w-5 h-5" /> Voltar
       </Link>
       <p className="text-md font-normal">Adicionar na comanda</p>
-      <div className="flex flex-wrap gap-5 w-[350px]">
-        <div className="flex justify-between gap-4 max-md:w-full shadow-xl p-4 border border-zinc-200 rounded-lg">
-          <div className="flex gap-3">
-            <Image
-              src="https://cdn.pixabay.com/photo/2016/03/05/23/02/barbecue-1239434_1280.jpg"
-              alt="image"
-              width={150}
-              height={150}
-              className="rounded-lg"
-            />
-            <div className="flex flex-col justify-evenly">
-              <p className="font-medium text-lg truncate">Pão</p>
-              <p className="font-medium text-md">R$ 12</p>
+      <div className="flex flex-wrap gap-5">
+        {data.map((itens, index) => (
+          <div
+            className="flex justify-between gap-3 max-md:w-full shadow-xl p-2 border md:min-w-[320px] border-zinc-200 rounded-lg"
+            key={index}
+          >
+            <div className="flex gap-3">
+              <Image
+                src={itens.urlImage}
+                alt={`Imagem do ${itens.title}`}
+                width={150}
+                height={150}
+                className="rounded-lg"
+              />
+              <div className="flex flex-col justify-evenly">
+                <p className="font-medium text-lg truncate">{itens.title}</p>
+                <p className="font-medium text-md">
+                  {itens.value.toLocaleString('pt-br', {
+                    style: 'currency',
+                    currency: 'BRL',
+                    minimumFractionDigits: 0,
+                    maximumFractionDigits: 2,
+                  })}
+                </p>
+              </div>
             </div>
+            <AddCartComanda id={itens.idItens} />
           </div>
-          <div title="adicionar item" className="">
-            <Plus className="w-5 h-5 rounded-full border border-zinc-800 hover:bg-zinc-700 hover:text-white" />
-          </div>
-        </div>
+        ))}
       </div>
       <div>
         <BtnForm title="Adicionar" />
