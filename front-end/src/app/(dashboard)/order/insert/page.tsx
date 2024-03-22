97e2f8e004b99d51b34b57e2b538654b67ff90b2
Andre <andrealbuquerque2001@gmail.com>
2024-03-22 12:46:44 -0300

    feat: improvement page insert order

--- front-end/src/app/(dashboard)/order/insert/page.tsx
+++ front-end/src/app/(dashboard)/order/insert/page.tsx
@@ -1,8 +1,109 @@
+'use client'
 import { BtnForm } from '@/components/btnForm'
+import Api from '@/data/api'
+import { InterfaceItens } from '@/data/type/interfaceItens'
 import { ArrowLeft } from 'lucide-react'
 import Link from 'next/link'
+import { useSearchParams } from 'next/navigation'
+import { FormEvent, useEffect, useState } from 'react'
+
+async function insertOrder(body: object) {
+  try {
+    const request = await Api('/order/insert', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify(body),
+    })
+    const reqBody = await request.json()
+    return reqBody.data
+  } catch (error) {
+    console.error(error)
+    throw error
+  }
+}
+
+async function getItem(id: number): Promise<InterfaceItens> {
+  try {
+    const request = await Api(`/itens/${id}`, {
+      method: 'GET',
+      headers: {
+        'Content-type': 'application/json',
+      },
+    })
+    const reqBody = await request.json()
+    return reqBody.data.data
+  } catch (error) {
+    console.error(error)
+    throw error
+  }
+}
 
 export default function InsertOrder() {
+  const [data, setData] = useState<InterfaceItens>()
+  const [dados, setDados] = useState({
+    desconto: '',
+    qtdOrder: '',
+    tip: '',
+    valueOrder: 0,
+  })
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>,
+  ) => {
+    const { name, value } = e.target
+    setDados((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }))
+  }
+
+  const searchParams = useSearchParams()
+
+  const itens = searchParams.get('iten')
+  const menu = searchParams.get('menu')
+
+  function calculateValurProduct() {
+    let total = 0
+    let desconto = 0
+    for (let i = 0; i < Number(dados.qtdOrder); i++) {
+      if (data?.value !== undefined) total += data?.value
+    }
+    if (dados.desconto) {
+      desconto = total * (Number(dados.desconto) / 100)
+    }
+
+    if (dados.tip) total += Number(dados.tip)
+
+    if (total - desconto > 0) return total - desconto
+    else return 0
+  }
+
+  dados.valueOrder = calculateValurProduct()
+
+  useEffect(() => {
+    const handleData = async () => {
+      const dt = await getItem(Number(itens))
+      setData(dt)
+    }
+    handleData()
+  }, [])
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    if (itens && menu) {
+      const formData = new FormData(e.currentTarget)
+      formData.append('idItens', itens)
+      formData.append('idMenu', menu)
+      const objet = Object.fromEntries(formData)
+      const req = await insertOrder(objet)
+      if (req.message === 'sucess') {
+        alert('Item inserido com sucesso!')
+        window.history.back()
+      }
+    }
+  }
   return (
     <div className="flex flex-col mx-auto justify-center h-[80%] w-full items-center">
       <Link
@@ -13,42 +114,78 @@ export default function InsertOrder() {
         Voltar
       </Link>
       <p className="text-xl mb-1 w-96 max-md:mb-0 max-md:w-80">
-        Cadastro do pedido
+        Cadastrado do pedido
       </p>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="flex flex-col mt-3 max-md:mt-3">
-          <label htmlFor="idMenu">
-            Qual mesa: <span className="text-red-600">*</span>{' '}
-          </label>
+          <label>Item:</label>
           <input
-            type="text"
-            name="idMenu"
-            id="idMenu"
-            className="w-96 h-9 border border-zinc-400 rounded-[5px] max-md:w-80 px-2"
+            readOnly
+            defaultValue={data?.title}
+            className="w-96 h-9 border border-zinc-400 rounded-[5px] max-md:w-80 px-2 bg-zinc-200 outline-none"
             required
           />
         </div>
         <div className="flex flex-col mt-3 max-md:mt-3">
-          <label htmlFor="idItens">
-            Qual item: <span className="text-red-600">*</span>{' '}
-          </label>
+          <label>Preço do item:</label>
           <input
-            type="text"
-            name="idItens"
-            id="idItens"
-            className="w-96 h-9 border border-zinc-400 rounded-[5px] max-md:w-80 px-2"
+            readOnly
+            defaultValue={data?.value}
+            className="w-96 h-9 border border-zinc-400 rounded-[5px] max-md:w-80 px-2 bg-zinc-200 outline-none"
             required
           />
         </div>
         <div className="flex flex-col mt-3 max-md:mt-3">
           <label htmlFor="qtdOrder">
-            Quantos itens: <span className="text-red-600">*</span>{' '}
+            Quantidade de itens: <span className="text-red-600">*</span>{' '}
           </label>
           <input
             type="text"
             name="qtdOrder"
             id="qtdOrder"
             className="w-96 h-9 border border-zinc-400 rounded-[5px] max-md:w-80 px-2"
+            value={dados.qtdOrder ?? ''}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="flex flex-col mt-3 max-md:mt-3">
+          <label htmlFor="desconto">
+            Desconto(%): <span className="text-red-600">*</span>{' '}
+          </label>
+          <input
+            type="text"
+            name="desconto"
+            id="desconto"
+            className="w-96 h-9 border border-zinc-400 rounded-[5px] max-md:w-80 px-2"
+            value={dados.desconto ?? ''}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="flex flex-col mt-3 max-md:mt-3">
+          <label htmlFor="tip">
+            Gorjeta: <span className="text-red-600">*</span>{' '}
+          </label>
+          <input
+            type="text"
+            name="tip"
+            id="tip"
+            className="w-96 h-9 border border-zinc-400 rounded-[5px] max-md:w-80 px-2"
+            value={dados.tip ?? ''}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="flex flex-col mt-3 max-md:mt-3">
+          <label htmlFor="valueOrder">
+            Valor do pedido: <span className="text-red-600">*</span>{' '}
+          </label>
+          <input
+            type="text"
+            name="valueOrder"
+            id="valueOrder"
+            className="w-96 h-9 border border-zinc-400 rounded-[5px] max-md:w-80 px-2 bg-zinc-200 outline-none"
+            value={dados.valueOrder ?? ''}
+            readOnly
             required
           />
         </div>
