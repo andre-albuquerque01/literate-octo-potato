deb8669af6626490f6eeb075428a588567c38535
Andre <andrealbuquerque2001@gmail.com>
2024-03-22 12:47:11 -0300

    feat: new route, post and delete

---
+++ front-end/src/app/api/order/delete/[id]/route.ts
@@ -0,0 +1,32 @@
+import ApiRoute from '@/data/apiRoute'
+import { cookies } from 'next/headers'
+import { NextResponse } from 'next/server'
+
+export async function DELETE(
+  _: Request,
+  { params }: { params: { id: number } },
+) {
+  try {
+    const id = params.id
+    const cookiesStore = cookies()
+    const token = cookiesStore.get('token')
+
+    const response = await ApiRoute(`/order/${id}`, {
+      method: 'DELETE',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token?.value}`,
+      },
+      cache: 'no-cache',
+    })
+
+    const data = await response.json()
+
+    return NextResponse.json({ data })
+  } catch (error) {
+    return new Response(JSON.stringify(error), {
+      status: 401,
+    })
+  }
+}
