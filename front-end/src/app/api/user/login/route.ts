42228dfbd8961bb9305ec0c8107fc121d7ce6550
Andre <andrealbuquerque2001@gmail.com>
2024-03-24 12:13:16 -0300

    feat: update security

--- front-end/src/app/api/user/login/route.ts
+++ front-end/src/app/api/user/login/route.ts
@@ -25,7 +25,9 @@ export async function POST(request: Request) {
     if (data.data.r === 'JesusIsKingADM') {
       cookiesStore.set('r', data.data.r, {
         expires: Date.now() + 2 * 60 * 60 * 1000,
+        httpOnly: true,
         secure: true,
+        sameSite: 'strict',
       })
     }
 
