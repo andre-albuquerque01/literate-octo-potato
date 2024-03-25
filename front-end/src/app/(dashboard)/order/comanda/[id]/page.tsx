5079e308f9246f9168e9b29c9cb5997f460b2336
Andre <andrealbuquerque2001@gmail.com>
2024-03-25 13:08:56 -0300

    feat: improvement page

--- front-end/src/app/(dashboard)/order/comanda/[id]/page.tsx
+++ front-end/src/app/(dashboard)/order/comanda/[id]/page.tsx
@@ -22,8 +22,9 @@ export default async function Comanda({ params }: { params: { id: number } }) {
   let soma = 0
   let mesa = 0
   let date = ''
+  let dateUpdated = ''
 
-  data.forEach((sm) => {
+  data.forEach((sm: any) => {
     soma += sm.valueOrder
     mesa = sm.numberMesa
     date = sm.created_at
@@ -32,33 +33,37 @@ export default async function Comanda({ params }: { params: { id: number } }) {
     }
     if (date !== sm.created_at) {
       date = sm.created_at
+      dateUpdated = sm.updated_at
     }
   })
 
   function formatarData(dataISO: string): string {
-    const data = new Date(dataISO)
-
     function padLeft(value: number): string {
       return value < 10 ? '0' + value : value.toString()
     }
 
-    const dia = padLeft(data.getDate())
-    const mes = padLeft(data.getMonth() + 1)
-    const ano = data.getFullYear()
-    const hora = padLeft(data.getHours())
-    const minuto = padLeft(data.getMinutes())
+    if (dataISO) {
+      const data = new Date(dataISO)
 
-    return `${hora}:${minuto} - ${dia}/${mes}/${ano}`
+      const dia = padLeft(data.getDate())
+      const mes = padLeft(data.getMonth() + 1)
+      const ano = data.getFullYear()
+      const hora = padLeft(data.getHours())
+      const minuto = padLeft(data.getMinutes())
+
+      return `${hora}:${minuto} - ${dia}/${mes}/${ano}`
+    }
+    return ''
   }
 
   return (
     <div className="w-full">
-      <div className="max-md:w-[80%] max-md:mx-auto md:mt-4">
+      <div className="max-md:w-[90%] min-h-full max-md:mx-auto md:mt-4">
         <Link href="" className="flex items-center py-4 md:hidden">
           <ArrowLeft className="w-5 h-5" /> Voltar
         </Link>
         <h1 className="text-2xl">Comanda da mesa 1</h1>
-        <div className="flex flex-wrap max-md:justify-center gap-5">
+        <div className="flex flex-wrap max-md:justify-center gap-5 md:mt-4">
           {data.map((itens, index) => (
             <div
               className="flex justify-between gap-3 max-md:w-full shadow-xl p-2 border md:min-w-[320px] border-zinc-200 rounded-lg"
@@ -84,23 +89,23 @@ export default async function Comanda({ params }: { params: { id: number } }) {
                   </p>
                 </div>
               </div>
-              <RemoveItenComanda id={itens.idItens} />
+              <RemoveItenComanda id={itens.idOrder} />
             </div>
           ))}
         </div>
         <Link
           href={`/order/addCart/${params.id}`}
-          className="flex items-center gap-2 opacity-70 justify-center mt-5"
+          className="flex items-center gap-2 opacity-70 mx-auto w-48 justify-center mt-5 border-b-2 hover:border-red-600"
         >
           <Plus className="w-5 h-5 border border-black rounded-full" />
           <span>Adicionar mais itens.</span>
         </Link>
       </div>
       <div className="flex justify-center">
-        <div className="border border-zinc-500 fixed bottom-0 max-md:z-30 px-4 md:py-5 max-md:bg-white max-md:w-full md:mb-5 md:w-[40%] space-y-1 md:rounded-xl">
+        <div className="border border-zinc-500 max-md:fixed md:mt-5 bottom-0 max-md:z-30 px-4 md:py-5 bg-white max-md:w-full md:mb-5 md:w-[40%] space-y-1 md:rounded-xl">
           <div className="flex justify-between">
             <span className="font-medium">Data</span>
-            <span>{formatarData(date)}</span>
+            <span>{formatarData(dateUpdated)}</span>
           </div>
           <div className="flex justify-between">
             <span className="font-medium">Mesa</span>
@@ -118,9 +123,12 @@ export default async function Comanda({ params }: { params: { id: number } }) {
             </span>
           </div>
           <div className="flex justify-center">
-            <button className="mx-auto font-semibold w-52 h-10 bg-red-600 text-zinc-50 text-xl rounded-[9px] mt-3 max-md:w-44 max-md:mb-1 hover:bg-red-500">
+            <Link
+              href={`/menu/update/${params.id}?value=${soma}`}
+              className="flex justify-center items-center font-semibold w-52 h-10 bg-red-600 text-zinc-50 text-xl rounded-[9px] mt-3 max-md:w-44 max-md:mb-1 hover:bg-red-500"
+            >
               Finalizar
-            </button>
+            </Link>
           </div>
         </div>
       </div>
