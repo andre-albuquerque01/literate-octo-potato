53ff5ac424f799201f441c014e4de6587a057e6b
Andre <andrealbuquerque2001@gmail.com>
2024-03-26 13:00:34 -0300

    feat: fake data

--- back-end/database/factories/ItensFactory.php
+++ back-end/database/factories/ItensFactory.php
@@ -3,7 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-
+use Illuminate\Support\Str;
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Itens>
  */
@@ -17,7 +17,17 @@ class ItensFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'codigo' => Str::random(10),
+            'title' => fake()->name(),
+            'desc'  => fake()->text(50),
+            'value' => fake()->randomNumber(3),
+            'statusIten' => 1,
+            'qtdIten' => fake()->randomNumber(),
+            'slug' => fake()->name(),
+            'urlImage' => $this->faker->imageUrl(480, 480),
+            'idCategory' => fake()->numberBetween(5, 12),
+            'waitTime' => fake()->randomNumber(1).' - '.fake()->randomNumber(2).' minutes',
+            'position'=> 'entrada'
         ];
     }
 }
