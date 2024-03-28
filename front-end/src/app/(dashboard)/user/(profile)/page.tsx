56f6ad7821b7302c03d43012e69576c6ea5639d4
Andre <andrealbuquerque2001@gmail.com>
2024-03-28 12:38:21 -0300

    feat: improvement page, autorization

--- front-end/src/app/(dashboard)/user/(profile)/page.tsx
+++ front-end/src/app/(dashboard)/user/(profile)/page.tsx
@@ -1,5 +1,6 @@
 import {
   Apple,
+  BookAIcon,
   History,
   Home,
   ListIcon,
@@ -7,9 +8,12 @@ import {
   Search,
   SquarePen,
 } from 'lucide-react'
+import { cookies } from 'next/headers'
 import Link from 'next/link'
 
 export default function User() {
+  const cookieStore = cookies()
+  const r = cookieStore.has('r')
   return (
     <div className="mt-6 space-y-3 w-36 max-md:p-8 max-md:mt-0 max-md:w-56">
       <Link href="/itens/search" className="flex items-center gap-2 md:hidden">
@@ -24,14 +28,29 @@ export default function User() {
         <SquarePen className="h-5 w-5" />
         Editar o perfil
       </Link>
-      <Link href="/order/menu" className="flex items-center gap-2">
+      <Link
+        href="/order/list"
+        className={`flex items-center gap-2 ${r && 'hidden'}`}
+      >
         <ListIcon className="h-5 w-5" />
         Pedido
       </Link>
-      <Link href="/itens/list" className="flex items-center gap-2">
-        <Apple className="h-5 w-5" />
-        Itens
-      </Link>
+      {cookieStore.has('r') && (
+        <>
+          <Link href="/itens/list" className="flex items-center gap-2">
+            <Apple className="h-5 w-5" />
+            Itens
+          </Link>
+          <Link href="/menu/list" className="flex items-center gap-2">
+            <BookAIcon className="h-5 w-5" />
+            Pedidos do cliente
+          </Link>
+          <Link href="/category/list" className="flex items-center gap-2">
+            <BookAIcon className="h-5 w-5" />
+            Categorias
+          </Link>
+        </>
+      )}
       <Link href="/order/historic" className="flex items-center gap-2">
         <History className="h-5 w-5" />
         Historico
