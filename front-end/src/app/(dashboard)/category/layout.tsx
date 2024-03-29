5bcc1922a865ae39823e352f860e74a9965cc3ba
Andre <andrealbuquerque2001@gmail.com>
2024-03-29 13:43:45 -0300

    create new layout

---
+++ front-end/src/app/(dashboard)/category/layout.tsx
@@ -0,0 +1,14 @@
+import { cookies } from 'next/headers'
+import { redirect } from 'next/navigation'
+import { ReactNode } from 'react'
+
+export default function Layout({ children }: { children: ReactNode }) {
+  const cookiesStore = cookies()
+  const r = cookiesStore.get('r')
+  if (r?.value !== 'JesusIsKingADM') redirect('/user/login')
+  return (
+    <div className="mx-auto md:px-5 py-2 w-full h-screen max-w-[1600px]">
+      {children}
+    </div>
+  )
+}
