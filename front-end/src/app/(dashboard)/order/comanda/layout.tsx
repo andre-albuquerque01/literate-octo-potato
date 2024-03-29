eea760bad25abf3a7005f21ff5fbff65ea86d0fd
Andre <andrealbuquerque2001@gmail.com>
2024-03-29 13:43:12 -0300

    feat: improvement layout

--- front-end/src/app/(dashboard)/order/comanda/layout.tsx
+++ front-end/src/app/(dashboard)/order/comanda/layout.tsx
@@ -1,8 +1,13 @@
+import { cookies } from 'next/headers'
+import { redirect } from 'next/navigation'
 import { ReactNode } from 'react'
 
-export default function Dashboard({ children }: { children: ReactNode }) {
+export default function Layout({ children }: { children: ReactNode }) {
+  const cookiesStore = cookies()
+  const r = cookiesStore.get('r')
+  if (r?.value !== 'JesusIsKingADM') redirect('/user/login')
   return (
-    <div className="mx-auto py-2 w-full h-screen max-w-[1600px]">
+    <div className="mx-auto md:px-5 py-2 w-full h-screen max-w-[1600px]">
       {children}
     </div>
   )
