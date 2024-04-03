bc980cfebd3313a032d333f5edfc7dd1edc1b1b2
Andre <andrealbuquerque2001@gmail.com>
2024-04-03 15:08:27 -0300

    feat: new server data.table

---
+++ front-end/src/app/actions/table/getAllTable.tsx
@@ -0,0 +1,26 @@
+'use server'
+import ApiRoute from '@/data/apiRoute'
+import { cookies } from 'next/headers'
+import { NextResponse } from 'next/server'
+
+export default async function GetAllTableService() {
+  try {
+    const cookiesStore = cookies()
+    const token = cookiesStore.get('token')
+
+    const response = await ApiRoute(`/table`, {
+      cache: 'no-cache',
+      headers: {
+        Authorization: `Bearer ${token?.value}`,
+      },
+    })
+
+    const data = await response.json()
+
+    return NextResponse.json({ data })
+  } catch (error) {
+    return new Response(JSON.stringify('error'), {
+      status: 401,
+    })
+  }
+}
