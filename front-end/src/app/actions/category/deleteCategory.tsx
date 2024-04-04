afdf8cd9b6a74eef5aaded7c2149dbf15f00a74f
Andre <andrealbuquerque2001@gmail.com>
2024-04-04 11:04:34 -0300

    feat: create action category

---
+++ front-end/src/app/actions/category/deleteCategory.tsx
@@ -0,0 +1,27 @@
+'use server'
+
+import ApiRoute from '@/data/apiRoute'
+import { cookies } from 'next/headers'
+
+export async function DeleteCategory(id: number) {
+  try {
+    const cookiesStore = cookies()
+    const token = cookiesStore.get('token')
+
+    const response = await ApiRoute(`/category/${id}`, {
+      method: 'DELETE',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token?.value}`,
+      },
+    })
+
+    if (!response.ok) return false
+    return true
+  } catch (error) {
+    return new Response(JSON.stringify(error), {
+      status: 401,
+    })
+  }
+}
