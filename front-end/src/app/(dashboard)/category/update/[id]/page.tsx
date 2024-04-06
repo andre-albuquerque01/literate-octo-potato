3b28c48cf709ad1fa7504b5254254f7452a61152
Andre <andrealbuquerque2001@gmail.com>
2024-04-06 12:12:58 -0300

    feat: suspense

--- front-end/src/app/(dashboard)/category/update/[id]/page.tsx
+++ front-end/src/app/(dashboard)/category/update/[id]/page.tsx
@@ -6,7 +6,7 @@ import { CategoryInterface } from '@/data/type/category'
 import { ArrowLeft } from 'lucide-react'
 import Link from 'next/link'
 import { useRouter } from 'next/navigation'
-import { FormEvent, useEffect, useState } from 'react'
+import { FormEvent, Suspense, useEffect, useState } from 'react'
 
 async function getCategory(id: number) {
   try {
@@ -54,51 +54,55 @@ export default function UpdateCategoryPage({
   }
 
   return (
-    <div className="flex flex-col mx-auto justify-center min-h-[80%] w-full items-center">
-      {data?.typeCategory !== undefined ? (
-        <>
-          <Link
-            href="/category/list"
-            className="md:hidden flex items-center gap-1 text-sm mb-3 w-96 max-md:mt-24 max-md:w-80"
-          >
-            <ArrowLeft className="w-5 h-5" />
-            Voltar
-          </Link>
-          <p className="text-xl mb-1 w-96 max-md:mb-0 max-md:w-80">
-            Cadastro de categoria
-          </p>
-          <form onSubmit={handleSubmit}>
-            <div className="flex flex-col mt-3 max-md:mt-3">
-              <label htmlFor="typeCategory">
-                Nome da categoria: <span className="text-red-600">*</span>{' '}
-              </label>
-              <input
-                type="text"
-                name="typeCategory"
-                id="typeCategory"
-                className="w-96 h-9 border border-zinc-400 rounded-[5px] max-md:w-80 px-2"
-                defaultValue={data?.typeCategory ?? ''}
-                required
-              />
-            </div>
-            <div className="flex flex-col mt-3 max-md:mt-3">
-              <label htmlFor="urlImageCategory">
-                Imagem de fundo da categoria:
-              </label>
-              <input
-                type="text"
-                name="urlImageCategory"
-                id="urlImageCategory"
-                className="w-96 h-9 border border-zinc-400 rounded-[5px] max-md:w-80 px-2"
-                defaultValue={data?.urlImageCategory ?? ''}
-              />
-            </div>
-            <BtnForm title="Alterar" />
-          </form>
-        </>
-      ) : (
-        <h1>Categoria não encontrada!</h1>
-      )}
-    </div>
+    <>
+      <Suspense fallback={<p>Carregando...</p>}>
+        <div className="flex flex-col mx-auto justify-center min-h-[80%] w-full items-center">
+          {data?.typeCategory !== undefined ? (
+            <>
+              <Link
+                href="/category/list"
+                className="md:hidden flex items-center gap-1 text-sm mb-3 w-96 max-md:mt-24 max-md:w-80"
+              >
+                <ArrowLeft className="w-5 h-5" />
+                Voltar
+              </Link>
+              <p className="text-xl mb-1 w-96 max-md:mb-0 max-md:w-80">
+                Cadastro de categoria
+              </p>
+              <form onSubmit={handleSubmit}>
+                <div className="flex flex-col mt-3 max-md:mt-3">
+                  <label htmlFor="typeCategory">
+                    Nome da categoria: <span className="text-red-600">*</span>{' '}
+                  </label>
+                  <input
+                    type="text"
+                    name="typeCategory"
+                    id="typeCategory"
+                    className="w-96 h-9 border border-zinc-400 rounded-[5px] max-md:w-80 px-2"
+                    defaultValue={data?.typeCategory ?? ''}
+                    required
+                  />
+                </div>
+                <div className="flex flex-col mt-3 max-md:mt-3">
+                  <label htmlFor="urlImageCategory">
+                    Imagem de fundo da categoria:
+                  </label>
+                  <input
+                    type="text"
+                    name="urlImageCategory"
+                    id="urlImageCategory"
+                    className="w-96 h-9 border border-zinc-400 rounded-[5px] max-md:w-80 px-2"
+                    defaultValue={data?.urlImageCategory ?? ''}
+                  />
+                </div>
+                <BtnForm title="Alterar" />
+              </form>
+            </>
+          ) : (
+            <h1>Categoria não encontrada!</h1>
+          )}
+        </div>
+      </Suspense>
+    </>
   )
 }
