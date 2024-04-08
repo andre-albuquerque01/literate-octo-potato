e1a15665f8a19cee8d86c99b3d97fe14a82be65b
Andre <andrealbuquerque2001@gmail.com>
2024-04-08 13:11:21 -0300

    feat: update page

--- front-end/src/app/(dashboard)/itens/list/page.tsx
+++ front-end/src/app/(dashboard)/itens/list/page.tsx
@@ -1,15 +1,10 @@
+import { GetListItens } from '@/app/actions/itens/listItens'
 import LinkPagination from '@/components/LinkPagination'
 import { RemoveIten } from '@/components/removeItem'
-import Api from '@/data/api'
 import { InterfaceItens } from '@/data/type/interfaceItens'
 import { ArrowLeft, PenBoxIcon, PlusCircle } from 'lucide-react'
 import Image from 'next/image'
 import Link from 'next/link'
-
-interface FeaturedEventsResponse {
-  data: InterfaceItens[]
-  countPage: number
-}
 
 interface PropsSearchParams {
   searchParams: {
@@ -17,29 +12,13 @@ interface PropsSearchParams {
   }
 }
 
-async function getFeaturedEvents(
-  page: number,
-): Promise<FeaturedEventsResponse> {
-  try {
-    const response = await Api(`/itens/list/${page}`, {
-      next: {
-        revalidate: 60,
-      },
-    })
-    const reqJson = await response.json()
-    const countPage = reqJson.data.meta.last_page
-    const data = reqJson.data.data
-
-    return { data, countPage }
-  } catch (error) {
-    return { data: [], countPage: 0 }
-  }
-}
-
 export default async function ListItens({ searchParams }: PropsSearchParams) {
   let { page: query } = searchParams || 1
   if (query === undefined) query = 1
-  const { data, countPage } = await getFeaturedEvents(query)
+  const datas = await GetListItens(query)
+  const dt = await datas.json()
+  const data = dt.data
+  const countPage = dt.countPage
 
   return (
     <div className="w-full">
@@ -58,39 +37,42 @@ export default async function ListItens({ searchParams }: PropsSearchParams) {
           Lista de itens
         </p>
         <div className="flex flex-wrap max-md:justify-center gap-5 md:mt-4">
-          {data.map((itens, index) => (
-            <div
-              className="flex justify-between gap-3 max-md:w-full max-h-36 md:w-[360px] max-w-96 shadow-xl p-2 border md:min-w-[320px] border-zinc-200 rounded-lg"
-              key={index}
-            >
-              <div className="flex gap-3">
-                <Image
-                  src={itens.urlImage}
-                  alt={`Imagem do ${itens.title}`}
-                  width={150}
-                  height={150}
-                  className="rounded-lg max-h-[150px] max-w-[150px]"
-                />
-                <div className="flex flex-col justify-evenly">
-                  <p className="font-medium text-lg text-wrap">{itens.title}</p>
-                  <p className="font-medium text-md">
-                    {itens.value.toLocaleString('pt-br', {
-                      style: 'currency',
-                      currency: 'BRL',
-                      minimumFractionDigits: 0,
-                      maximumFractionDigits: 2,
-                    })}
-                  </p>
+          {data &&
+            data.map((itens: InterfaceItens, index: number) => (
+              <div
+                className="flex justify-between gap-3 max-md:w-full max-h-36 md:w-[360px] max-w-96 shadow-xl p-2 border md:min-w-[320px] border-zinc-200 rounded-lg"
+                key={index}
+              >
+                <div className="flex gap-3">
+                  <Image
+                    src={itens.urlImage}
+                    alt={`Imagem do ${itens.title}`}
+                    width={150}
+                    height={150}
+                    className="rounded-lg max-h-[150px] max-w-[150px]"
+                  />
+                  <div className="flex flex-col justify-evenly">
+                    <p className="font-medium text-lg text-wrap">
+                      {itens.title}
+                    </p>
+                    <p className="font-medium text-md">
+                      {itens.value.toLocaleString('pt-br', {
+                        style: 'currency',
+                        currency: 'BRL',
+                        minimumFractionDigits: 0,
+                        maximumFractionDigits: 2,
+                      })}
+                    </p>
+                  </div>
+                </div>
+                <div className="flex flex-col gap-3">
+                  <RemoveIten id={itens.idItens} />
+                  <Link href={`/itens/update/${itens.idItens}`}>
+                    <PenBoxIcon className="w-5 h-5 hover:text-zinc-600" />
+                  </Link>
                 </div>
               </div>
-              <div className="flex flex-col gap-3">
-                <RemoveIten id={itens.idItens} />
-                <Link href={`/itens/update/${itens.idItens}`}>
-                  <PenBoxIcon className="w-5 h-5 hover:text-zinc-600" />
-                </Link>
-              </div>
-            </div>
-          ))}
+            ))}
         </div>
       </div>
       <div className="flex justify-center mt-4 h-10">
--- front-end/src/app/(dashboard)/itens/search/(itens)/page.tsx
+++ front-end/src/app/(dashboard)/itens/search/(itens)/page.tsx
@@ -1,23 +1,11 @@
+import { GetAllCategory } from '@/app/actions/category/getAllCatgeory'
 import { CategorySearch } from '@/components/categorySearch'
 import { FormSearch } from '@/components/form-search'
-import Api from '@/data/api'
 import { CategoryInterface } from '@/data/type/category'
 
-async function getCategory(): Promise<CategoryInterface[]> {
-  try {
-    const request = await Api('/category/getAll', {
-      cache: 'no-cache',
-    })
-    const reqJson = await request.json()
-    return reqJson.data.data
-  } catch (error) {
-    console.error(error)
-    throw error
-  }
-}
-
 export default async function SearchIten() {
-  const data = await getCategory()
+  const reqBody = await GetAllCategory()
+  const data = reqBody.data
 
   return (
     <div className="max-md:w-[390px] mx-auto space-y-5">
@@ -25,15 +13,17 @@ export default async function SearchIten() {
       <div className="space-y-3">
         <h1 className="font-normal text-[20px]">Categorias</h1>
         <div className="max-md:space-y-[19px] md:flex w-full flex-wrap gap-6 ">
-          {data.map((category, index) => (
-            <CategorySearch
-              key={index}
-              path={`/itens/search/itens?c=${category.typeCategory.toLowerCase()}`}
-              src={category.urlImageCategory ?? ''}
-              alt={`Imagem de fundo de ${category.typeCategory}`}
-              title={category.typeCategory}
-            />
-          ))}
+          {data &&
+            data.length > 0 &&
+            data.map((category: CategoryInterface, index: number) => (
+              <CategorySearch
+                key={index}
+                path={`/itens/search/itens?c=${category.typeCategory.toLowerCase()}`}
+                src={category.urlImageCategory ?? ''}
+                alt={`Imagem de fundo de ${category.typeCategory}`}
+                title={category.typeCategory}
+              />
+            ))}
         </div>
       </div>
     </div>
