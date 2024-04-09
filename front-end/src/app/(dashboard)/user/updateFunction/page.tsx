7e1c2541e3064d4e6e8a18821748d197c79c6738
Andre <andrealbuquerque2001@gmail.com>
2024-04-09 13:06:33 -0300

    feat: create new page

---
+++ front-end/src/app/(dashboard)/user/updateFunction/page.tsx
@@ -0,0 +1,68 @@
+'use client'
+import { UpdateFunction } from '@/app/actions/user/updateFunction'
+import { BtnForm } from '@/components/btnForm'
+import { ArrowLeft } from 'lucide-react'
+import Link from 'next/link'
+import { useRouter } from 'next/navigation'
+import { FormEvent } from 'react'
+
+export default function UpdateFunctionPage() {
+  const router = useRouter()
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    const formData = new FormData(e.currentTarget)
+    const data = Object.fromEntries(formData)
+    const req = await UpdateFunction(data)
+    if (req) {
+      alert('Alterado com sucesso!')
+      router.back()
+    } else {
+      alert('Não foi possível fazer alteração!')
+    }
+  }
+
+  return (
+    <div className="flex flex-col mx-auto justify-center min-h-[80%] w-full items-center">
+      <Link
+        href="/user"
+        className="md:hidden flex items-center gap-1 text-sm mb-3 w-96 max-md:mt-24 max-md:w-80"
+      >
+        <ArrowLeft className="w-5 h-5" />
+        Voltar
+      </Link>
+      <p className="text-xl mb-1 w-96 max-md:mb-0 max-md:w-80">
+        Alterar papel do usuário
+      </p>
+      <form onSubmit={handleSubmit}>
+        <div className="flex flex-col mt-3 max-md:mt-3">
+          <label htmlFor="cpf">
+            CPF do usuário: <span className="text-red-600">*</span>{' '}
+          </label>
+          <input
+            type="text"
+            name="cpf"
+            id="cpf"
+            className="w-96 h-9 border border-zinc-400 rounded-[5px] max-md:w-80 px-2"
+            required
+          />
+        </div>
+        <div className="flex flex-col mt-3">
+          <label htmlFor="papelRole">
+            Nova função: <span className="text-red-600">*</span>{' '}
+          </label>
+          <select
+            name="papelRole"
+            id="papelRole"
+            className="w-96 h-9 border border-zinc-400 bg-white rounded-[5px] max-md:w-80 px-2 uppercase"
+            required
+          >
+            <option value="">Selecione a função</option>
+            <option value="admin">Admin</option>
+            <option value="user">User</option>
+          </select>
+        </div>
+        <BtnForm title="Alterar" />
+      </form>
+    </div>
+  )
+}
