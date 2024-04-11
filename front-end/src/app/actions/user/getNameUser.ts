67b8e39e1dd889df2378f939b4fbb2bf6fbb13aa
Andre <andrealbuquerque2001@gmail.com>
2024-04-11 09:56:38 -0300

    feat: create new action

---
+++ front-end/src/app/actions/user/getNameUser.ts
@@ -0,0 +1,26 @@
+'use server'
+
+import ApiRoute from '@/data/apiRoute'
+import { cookies } from 'next/headers'
+
+export async function GetNameUser() {
+  try {
+    const cookiesStore = cookies()
+    const token = cookiesStore.get('token')
+
+    const response = await ApiRoute('/userName', {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token?.value}`,
+      },
+    })
+
+    const data = await response.json()
+
+    return data.name
+  } catch (error) {
+    return 'Error'
+  }
+}
