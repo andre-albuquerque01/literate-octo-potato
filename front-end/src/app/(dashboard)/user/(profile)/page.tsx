ab8ecedbc061e12c4a0fe6e993a2a7de82f14909
Andre <andrealbuquerque2001@gmail.com>
2024-04-11 09:56:11 -0300

    feat: improvement page

--- front-end/src/app/(dashboard)/user/(profile)/page.tsx
+++ front-end/src/app/(dashboard)/user/(profile)/page.tsx
@@ -1,3 +1,4 @@
+import { GetNameUser } from '@/app/actions/user/getNameUser'
 import { Logout } from '@/components/logout'
 import {
   Apple,
@@ -14,52 +15,85 @@ import {
 import { cookies } from 'next/headers'
 import Link from 'next/link'
 
-export default function User() {
+export default async function User() {
   const cookiesStore = cookies()
   const r = cookiesStore.get('r')
+  const data = await GetNameUser()
+
+  function getDate() {
+    let message = ''
+    const date = new Date()
+    const hours = date.getHours()
+    if (hours >= 0) message = `Bom dia, ${data}!`
+    else if (hours >= 12) message = `Boa tarde, ${data}!`
+    else if (hours >= 18) message = `Boa noite, ${data}!`
+    return message
+  }
 
   return (
     <div className="mt-6 space-y-3 w-36 max-md:p-8 max-md:mt-0 max-md:w-56">
-      <Link href="/itens/search" className="flex items-center gap-2 md:hidden">
+      <h1 className="">{getDate()}</h1>
+      <Link
+        href="/itens/search"
+        className="flex items-center gap-2 md:hidden hover:underline"
+      >
         <Home className="h-5 w-5" />
         Inicio
       </Link>
-      <Link href="/itens/search" className="flex items-center gap-2">
+      <Link
+        href="/itens/search"
+        className="flex items-center gap-2 hover:underline"
+      >
         <Search className="h-5 w-5" />
         Pesquisar
       </Link>
-      <Link href="/user/update" className="flex items-center gap-2">
+      <Link
+        href="/user/update"
+        className="flex items-center gap-2 hover:underline"
+      >
         <SquarePen className="h-5 w-5" />
         Editar o perfil
       </Link>
-      <Link href="/user/updatePasssword" className="flex items-center gap-2">
+      <Link
+        href="/user/updatePasssword"
+        className="flex items-center gap-2 hover:underline"
+      >
         <PenLine className="h-5 w-5" />
         Alterar senha
       </Link>
       <Link
         href="/order/list"
-        className={`flex items-center gap-2 ${r?.value && 'hidden'}`}
+        className={`flex items-center gap-2 hover:underline ${r?.value && 'hidden'}`}
       >
         <ListIcon className="h-5 w-5" />
         Pedido
       </Link>
       {r?.value === 'JesusIsKingADM' && (
         <>
-          <Link href="/itens/list" className="flex items-center gap-2">
+          <Link
+            href="/itens/list"
+            className="flex items-center gap-2 hover:underline"
+          >
             <Apple className="h-5 w-5" />
             Itens
           </Link>
-          <Link href="/menu/list" className="flex items-center w-[170px] gap-2">
+          <Link
+            href="/menu/list"
+            className="flex items-center w-[170px] gap-2 hover:underline"
+          >
             <BookAIcon className="h-5 w-5" />
             Pedidos do cliente
           </Link>
-          <Link href="/category/list" className="flex items-center gap-2">
+          <Link
+            href="/category/list"
+            className="flex items-center gap-2 hover:underline"
+          >
             <BookOpenCheck className="h-5 w-5" />
             Categorias
           </Link>
           <Link
             href="/user/updateFunction"
-            className="flex items-center gap-2 w-48"
+            className="flex items-center gap-2 hover:underline w-48"
           >
             <PlusCircle className="h-5 w-5" />
             Adicionar funcionário
@@ -68,7 +102,7 @@ export default function User() {
       )}
       <Link
         href="/order/historic"
-        className={`flex items-center gap-2 ${r?.value && 'hidden'}`}
+        className={`flex items-center gap-2 hover:underline ${r?.value && 'hidden'}`}
       >
         <History className="h-5 w-5" />
         Historico
