cfd6de282a2e016841bf29a958abccf90bc39dcb
Andre <andrealbuquerque2001@gmail.com>
2024-04-15 10:02:55 -0300

    feat: improvement in action

--- front-end/src/app/actions/itens/listItens.tsx
+++ front-end/src/app/actions/itens/listItens.tsx
@@ -4,16 +4,20 @@ import ApiRoute from '@/data/apiRoute'
 import { NextResponse } from 'next/server'
 
 export async function GetListItens(page: number) {
-  const response = await ApiRoute(`/itensa?page=${page}`, {
-    next: {
-      revalidate: 60,
-      tags: ['itens'],
-    },
-  })
-  const datas = await response.json()
+  try {
+    const response = await ApiRoute(`/itensa?page=${page}`, {
+      next: {
+        revalidate: 60,
+        tags: ['itens'],
+      },
+    })
+    const datas = await response.json()
 
-  const countPage = datas.meta.last_page
-  const data = datas.data
+    const countPage = datas.meta.last_page
+    const data = datas.data
 
-  return NextResponse.json({ data, countPage })
+    return NextResponse.json({ data, countPage })
+  } catch (error) {
+    return NextResponse.json({ error: 'error' })
+  }
 }
