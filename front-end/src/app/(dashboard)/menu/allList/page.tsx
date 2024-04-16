bcef6a80fc7dad6981192f8457bdc69a234a1169
Andre <andrealbuquerque2001@gmail.com>
2024-04-16 11:45:05 -0300

    feat: improvement in page

--- front-end/src/app/(dashboard)/menu/allList/page.tsx
+++ front-end/src/app/(dashboard)/menu/allList/page.tsx
@@ -1,5 +1,8 @@
 import { GetAllListMenu } from '@/app/actions/menu/getAllListMenu'
+import { GetCpfMenu } from '@/app/actions/menu/getCpfMenu'
 import LinkPagination from '@/components/LinkPagination'
+import LinkPaginationQuery from '@/components/LinkPaginationQuery'
+import { FormSearchMenuCPF } from '@/components/form-search-menuCpf'
 import { MenuInterface } from '@/data/type/menu'
 import { ArrowLeft, Edit, EyeIcon } from 'lucide-react'
 import Link from 'next/link'
@@ -7,16 +10,54 @@ import Link from 'next/link'
 interface PropsSearchParams {
   searchParams: {
     page: number
+    q: string
   }
 }
 
 export default async function allListMenu({ searchParams }: PropsSearchParams) {
-  let { page: query } = searchParams || 1
-  if (query === undefined) query = 1
-  const datas = await GetAllListMenu(query)
-  const dt = await datas.json()
-  const data = dt.data
-  const countPage = dt.countPage
+  const { q: queryQ } = searchParams
+
+  let title = ''
+  let { page } = searchParams || 1
+  if (page === undefined) page = 1
+
+  let data = []
+  let countPage = 0
+
+  if (queryQ && queryQ !== '') {
+    const datas = await GetCpfMenu(queryQ, page)
+    const dt = await datas.json()
+    data = dt.data
+    countPage = dt.countPage
+    title = `CPF pesquisado: ${queryQ}`
+  } else {
+    const datas = await GetAllListMenu(page)
+    const dt = await datas.json()
+    data = dt.data
+    countPage = dt.countPage
+    title = 'Todos os pedidos.'
+  }
+
+  function VerifyQuery() {
+    if (queryQ && queryQ !== '') {
+      return (
+        <>
+          <LinkPaginationQuery
+            path={queryQ}
+            query={page}
+            letter={'q'}
+            countPage={countPage}
+          />
+        </>
+      )
+    } else {
+      return (
+        <>
+          <LinkPagination query={page} countPage={countPage} />
+        </>
+      )
+    }
+  }
 
   return (
     <div className="flex flex-col min-h-[90%] w-full px-3">
@@ -27,8 +68,10 @@ export default async function allListMenu({ searchParams }: PropsSearchParams) {
         <ArrowLeft className="w-5 h-5" />
         Voltar
       </Link>
+      <FormSearchMenuCPF />
+      <div className="text-sm mt-5">{title && title}</div>
       <p className="text-xl mb-1 max-md:mb-0 mt-5">
-        Lista de pedido, tanto aberto como fechados
+        Lista de todos pedido, tanto aberto como fechados.
       </p>
       <div className="space-y-5 mt-4">
         {data !== undefined && data.length > 0 ? (
@@ -62,9 +105,7 @@ export default async function allListMenu({ searchParams }: PropsSearchParams) {
         )}
       </div>
       <div className="max-md:h-28">
-        <div className="flex justify-center mt-4 h-10">
-          <LinkPagination query={query} countPage={countPage} />
-        </div>
+        <div className="flex justify-center mt-4 h-10">{VerifyQuery()}</div>
       </div>
     </div>
   )
