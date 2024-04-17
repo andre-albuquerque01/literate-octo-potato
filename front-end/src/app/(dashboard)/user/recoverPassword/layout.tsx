65715e941b5beef4bc14c2f80716657d57528a70
Andre <andrealbuquerque2001@gmail.com>
2024-04-17 12:00:02 -0300

    finish project

--- front-end/src/app/(dashboard)/user/recoverPassword/layout.tsx
+++ front-end/src/app/(dashboard)/user/recoverPassword/layout.tsx
@@ -2,6 +2,7 @@ import { ReactNode } from 'react'
 import { Metadata } from 'next'
 import { cookies } from 'next/headers'
 import { redirect } from 'next/navigation'
+import { ReCaptchaProvider } from 'next-recaptcha-v3'
 
 export const metadata: Metadata = {
   title: 'Recuperação de senha',
@@ -11,5 +12,13 @@ export default function User({ children }: { children: ReactNode }) {
   const cookiesStore = cookies()
   const token = cookiesStore.has('token')
   if (token) redirect('/user')
-  return <div>{children}</div>
+  return (
+    <div>
+      <ReCaptchaProvider
+        reCaptchaKey={process.env.NEXT_PUBLIC_API_KEY_RECAPTCHA}
+      >
+        {children}
+      </ReCaptchaProvider>
+    </div>
+  )
 }
