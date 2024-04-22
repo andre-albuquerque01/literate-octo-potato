6481246bcbc913d6a954ee88a76031d3a91841ed
Andre <andrealbuquerque2001@gmail.com>
2024-04-22 13:58:51 -0300

    feat: create new event,

--- back-end/app/Providers/EventServiceProvider.php
+++ back-end/app/Providers/EventServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use App\Events\UserRecoverPassword;
 use App\Events\UserRegistered;
+use App\Listeners\SendRecoverEmail;
 use App\Listeners\SendVerifyEmail;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
@@ -19,6 +21,9 @@ class EventServiceProvider extends ServiceProvider
     protected $listen = [
         UserRegistered::class =>[
             SendVerifyEmail::class,
+        ],
+        UserRecoverPassword::class =>[
+            SendRecoverEmail::class,
         ],
     ];
 
