cd6da90141674f0f4f2025485fd4d0f264dc0976
andre <andrealbuquerque2001@gmail.com>
2024-07-31 11:15:14 -0300

    feat: update  resource

--- back-end/app/Http/Resources/CategoryResource.php
+++ back-end/app/Http/Resources/CategoryResource.php
@@ -14,6 +14,10 @@ class CategoryResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        // return parent::toArray($request);
+        return [
+            "idCategory" => $this->idCategory,
+            "typeCategory" => $this->typeCategory,
+        ];
     }
 }
