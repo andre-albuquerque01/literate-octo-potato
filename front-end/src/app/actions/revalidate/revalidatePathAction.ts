374aacc371a4fa7dd8c800b2b85e79ca17d52460
andre <andrealbuquerque2001@gmail.com>
2024-08-02 11:50:43 -0300

    feat: delete files

--- front-end/src/app/actions/revalidate/revalidatePathAction.ts
+++
@@ -1,5 +0,0 @@
-import { revalidatePath } from 'next/cache'
-
-export async function revalidatePathAction(path: string) {
-  revalidatePath(path)
-}
--- front-end/src/data/api.ts
+++
@@ -1,7 +0,0 @@
-export default function Api(path: string, init?: RequestInit) {
-  const baseUrl = 'http://localhost:3000'
-  const apiPrefix = '/api'
-  const url = new URL(apiPrefix.concat(path), baseUrl)
-
-  return fetch(url, init)
-}
