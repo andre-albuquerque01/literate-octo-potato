6329caac63b6dd0fa99b551b560ea2b7423e9e68
andre <andrealbuquerque2001@gmail.com>
2024-08-02 11:52:40 -0300

    feat: update action

---
+++ front-end/src/actions/category/deleteCategory.tsx
@@ -0,0 +1,25 @@
+'use server'
+
+import ApiRoute from '@/data/apiRoute'
+import { cookies } from 'next/headers'
+import { revalidateTag } from 'next/cache'
+
+export async function DeleteCategory(id: number) {
+  try {
+    const response = await ApiRoute(`/category/${id}`, {
+      method: 'DELETE',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${cookies().get('token')?.value}`,
+      },
+    })
+    revalidateTag('category')
+    if (!response.ok) return false
+    return true
+  } catch (error) {
+    return new Response(JSON.stringify(error), {
+      status: 401,
+    })
+  }
+}
