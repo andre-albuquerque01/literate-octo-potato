7c2a5f788ad5b1cd12796b054716e9341b96693f
andre <andrealbuquerque2001@gmail.com>
2024-08-02 11:53:06 -0300

    feat: update action

---
+++ front-end/src/actions/itens/getIdItens.tsx
@@ -0,0 +1,24 @@
+'use server'
+
+import ApiRoute from '@/data/apiRoute'
+
+export async function GetIdItens(id: number) {
+  try {
+    const response = await ApiRoute(`/itens/${id}`, {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      next: {
+        revalidate: 60,
+        tags: ['itens', 'rate'],
+      },
+    })
+
+    const data = await response.json()
+    return data.data
+  } catch (error) {
+    return 'Houver erro'
+  }
+}
