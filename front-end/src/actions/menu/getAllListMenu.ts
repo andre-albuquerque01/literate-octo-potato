73d89e5c004d2c285e8ad2c342b704fd50b9b412
andre <andrealbuquerque2001@gmail.com>
2024-08-05 10:46:51 -0300

    feat: update action

---
+++ front-end/src/actions/menu/getAllListMenu.ts
@@ -0,0 +1,29 @@
+'use server'
+
+import ApiRoute from '@/data/apiRoute'
+import { cookies } from 'next/headers'
+
+export async function GetAllListMenu(page: number) {
+  try {
+    const response = await ApiRoute(`/menuA?page=${page}`, {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${cookies().get('token')?.value}`,
+      },
+      next: {
+        revalidate: 30,
+        tags: ['menuList'],
+      },
+    })
+    const datas = await response.json()
+
+    const countPage = datas.meta.last_page
+    const data = datas.data
+
+    return { data, countPage }
+  } catch (error) {
+    return { data: [], countPage: 0 }
+  }
+}
