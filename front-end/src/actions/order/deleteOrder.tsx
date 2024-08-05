d43911e73c9fead0c61ad6d9538f5e579b4cbadc
andre <andrealbuquerque2001@gmail.com>
2024-08-05 10:47:07 -0300

    feat: update action

---
+++ front-end/src/actions/order/deleteOrder.tsx
@@ -0,0 +1,27 @@
+'use server'
+
+import ApiRoute from '@/data/apiRoute'
+import { cookies } from 'next/headers'
+import { revalidatePathAction } from '../revalidate/revalidatePathAction'
+import { revalidateTag } from 'next/cache'
+
+export async function DeleteOrder(id: number, idComanda: number) {
+  try {
+    const response = await ApiRoute(`/order/${id}`, {
+      method: 'DELETE',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${cookies().get('token')?.value}`,
+      },
+    })
+    if (!response.ok) return false
+    revalidatePathAction(`/order/comanda/${idComanda}`)
+    revalidateTag('order')
+    return true
+  } catch (error) {
+    return new Response(JSON.stringify(error), {
+      status: 401,
+    })
+  }
+}
