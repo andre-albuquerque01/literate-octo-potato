ce11d3e5a0c35c1ce1b3556aaeca9fa59b392f42
andre <andrealbuquerque2001@gmail.com>
2024-08-06 10:42:59 -0300

    feat: update middleware

--- front-end/src/middleware.ts
+++ front-end/src/middleware.ts
@@ -3,25 +3,20 @@ import { NextRequest, NextResponse } from 'next/server'
 export async function middleware(request: NextRequest) {
   const token = request.cookies.get('token')?.value
   const r = request.cookies.get('r')?.value
-  const authenticated = !!token
-  const autorization = !!r
+  const authenticated =
+    token !== undefined && token.length >= 49 && token.length <= 53
+  const autorization = r === 'JesusIsKingADM'
+  if (
+    !autorization &&
+    (request.nextUrl.pathname.startsWith('/menu') ||
+      request.nextUrl.pathname.startsWith('/table') ||
+      request.nextUrl.pathname.startsWith('/itens/insert') ||
+      request.nextUrl.pathname.startsWith('/itens/update') ||
+      request.nextUrl.pathname.startsWith('/category'))
+  ) {
+    return NextResponse.redirect(new URL('/user/', request.url))
+  }
 
-  if (
-    !autorization &&
-    (request.nextUrl.pathname.startsWith('/menu') ||
-      request.nextUrl.pathname.startsWith('/table') ||
-      request.nextUrl.pathname.startsWith('/category'))
-  ) {
-    return NextResponse.redirect(new URL('/user/', request.url))
-  }
-  if (
-    !autorization &&
-    (request.nextUrl.pathname.startsWith('/menu') ||
-      request.nextUrl.pathname.startsWith('/table') ||
-      request.nextUrl.pathname.startsWith('/category'))
-  ) {
-    return NextResponse.redirect(new URL('/user/', request.url))
-  }
   if (
     !authenticated &&
     (request.nextUrl.pathname.startsWith('/menu') ||
