b2b5c5a324e80ceab42f6bda730f76e08869c2f7
andre <andrealbuquerque2001@gmail.com>
2024-08-06 10:43:11 -0300

    feat: update page

--- front-end/src/app/(dashboard)/category/list/page.tsx
+++ front-end/src/app/(dashboard)/category/list/page.tsx
@@ -1,13 +1,10 @@
-import { GetAllCategory } from '@/app/actions/category/getAllCatgeory'
-import { RemoveCategory } from '@/components/removeCategory'
-import { CategoryInterface } from '@/data/type/category'
-import { ArrowLeft, Edit, PlusCircle } from 'lucide-react'
+import { GetAllCategory } from '@/actions/category/getAllCatgeory'
+import { CategoryListComponent } from '@/components/categoryList'
+import { ArrowLeft, PlusCircle } from 'lucide-react'
 import Link from 'next/link'
 
 export default async function GetAll() {
-  const reqBody = await GetAllCategory()
-  const data = reqBody.data
-
+  const data = await GetAllCategory()
   return (
     <div className="flex flex-col min-h-[90%] w-full px-3">
       <Link
@@ -27,30 +24,7 @@ export default async function GetAll() {
       <p className="text-xl mb-1 w-96 max-md:mb-0 max-md:w-80 mt-5">
         Lista de categorias cadastradas
       </p>
-      <div className="space-y-5 mt-4">
-        {data &&
-          data.length > 0 &&
-          data.map((category: CategoryInterface, key: number) => (
-            <div className="" key={key}>
-              <div className="flex mx-auto justify-between">
-                <span className="max-w-96">{category.typeCategory}</span>
-                <span className="max-md:hidden truncate w-96 text-center">
-                  {category.urlImageCategory ?? 'Sem imagem'}
-                </span>
-                <div className="flex items-center gap-3">
-                  <Link
-                    href={`/category/update/${category.idCategory}`}
-                    title={`Editar o item, ${category.typeCategory}`}
-                  >
-                    <Edit className="w-5 h-5" />
-                  </Link>
-                  <RemoveCategory id={category.idCategory} />
-                </div>
-              </div>
-              <div className="w-full h-[1px] bg-zinc-600"></div>
-            </div>
-          ))}
-      </div>
+      <CategoryListComponent data={data} />
     </div>
   )
 }
