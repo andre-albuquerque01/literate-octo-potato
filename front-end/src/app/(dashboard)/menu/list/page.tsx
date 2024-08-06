d7b08f228ec55340b8cfd7de3d195fe70fc2537f
andre <andrealbuquerque2001@gmail.com>
2024-08-06 10:43:37 -0300

    feat: update page

--- front-end/src/app/(dashboard)/menu/list/page.tsx
+++ front-end/src/app/(dashboard)/menu/list/page.tsx
@@ -1,12 +1,10 @@
-import GetAllMenuService from '@/app/actions/menu/getAllMenu'
-import { MenuInterface } from '@/data/type/menu'
-import { ArrowLeft, Edit, EyeIcon, PlusCircle } from 'lucide-react'
+import GetAllMenuService from '@/actions/menu/getAllMenu'
+import { GetAllMenuComponent } from '@/components/getAllMenu'
+import { ArrowLeft, PlusCircle } from 'lucide-react'
 import Link from 'next/link'
 
 export default async function listMenu() {
-  const reqbody = await GetAllMenuService()
-  const dt = await reqbody.json()
-  const data = dt.data.data
+  const data = await GetAllMenuService()
 
   return (
     <div className="flex flex-col min-h-[90%] w-full px-3">
@@ -27,37 +25,7 @@ export default async function listMenu() {
       <p className="text-xl mb-1 w-96 max-md:mb-0 max-md:w-80 mt-5">
         Lista de comandas abertas
       </p>
-      <div className="space-y-5 mt-4">
-        {data !== undefined && data.length > 0 ? (
-          data.map((menu: MenuInterface, key: number) => (
-            <div key={key}>
-              <div className="flex mx-auto justify-between">
-                <span className="max-w-96">Mesa: {menu.numberMesa}</span>
-                <span className="max-md:hidden truncate w-96 text-center">
-                  CPF cliente: {menu.cpf}
-                </span>
-                <div className="flex items-center gap-3">
-                  <Link
-                    href={`/menu/update/${menu.idMenu}`}
-                    title={`Editar o item, ${menu.numberMesa}`}
-                  >
-                    <Edit className="w-5 h-5" />
-                  </Link>
-                  <Link
-                    href={`/order/comanda/${menu.idMenu}`}
-                    title="Mostra o pedido"
-                  >
-                    <EyeIcon className="w-5 h-5" />
-                  </Link>
-                </div>
-              </div>
-              <div className="w-full h-[1px] bg-zinc-600"></div>
-            </div>
-          ))
-        ) : (
-          <h1>Nenhuma comanda aberta!</h1>
-        )}
-      </div>
+      <GetAllMenuComponent data={data} />
     </div>
   )
 }
