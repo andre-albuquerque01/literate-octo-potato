55916dae56fab8b05f6382763d3d8b88257c2292
andre <andrealbuquerque2001@gmail.com>
2024-08-07 12:04:15 -0300

    feat: update page

--- front-end/src/app/(dashboard)/user/insert/page.tsx
+++ front-end/src/app/(dashboard)/user/insert/page.tsx
@@ -1,92 +1,36 @@
 'use client'
-import { InsertUser } from '@/app/actions/user/insertUser'
-import { validarCPF } from '@/data/function/validateCpf'
-import { ValidateForm } from '@/data/function/validateForm'
+import { InsertUser } from '@/actions/user/insertUser'
+import { BtnForm } from '@/components/btnForm'
 import { ArrowLeft } from 'lucide-react'
 import { ReCaptchaProvider } from 'next-recaptcha-v3'
 import Link from 'next/link'
-import { useRouter } from 'next/navigation'
-import { FormEvent, useState } from 'react'
+import { useFormState, useFormStatus } from 'react-dom'
+
+function FormButton() {
+  const { pending } = useFormStatus()
+
+  return (
+    <>
+      {pending ? (
+        <button
+          className="mx-auto font-semibold w-52 h-10 bg-red-600 text-zinc-50 text-xl rounded-[9px] mt-3 max-md:w-44 max-md:mb-5 hover:bg-red-500"
+          disabled={pending}
+        >
+          Cadastrando...
+        </button>
+      ) : (
+        <BtnForm title="Cadastrar" />
+      )}
+    </>
+  )
+}
 
 export default function InsertUserPage() {
-  const [returnError, setReturnError] = useState<string>('')
-  const [status, setStatus] = useState(false)
-  const router = useRouter()
-
-  const hasNumber = /\d/
-  const hasUpperCase = /[A-Z]/
-  const hasLowerCase = /[a-z]/
-  const hasSymbol = /[!@#$%^&*(),.?":{}|<>_=+]/
-  const hasMinLength = /^.{8,}$/
-
-  const err = [
-    'Senha precisa ter pelo menos um número.',
-    'Senha precisa ter pelo menos uma letra maiúscula.',
-    'Senha precisa ter pelo menos uma letra minúscula.',
-    'Senha precisa ter pelo menos um símbolo.',
-    'Senha precisa ter pelo menos 8 caracteres.',
-    'Senhas não correspondem',
-  ]
-
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  function validatePassword(password: any) {
-    if (!hasNumber.test(password)) return err[0]
-    if (!hasUpperCase.test(password)) return err[1]
-    if (!hasLowerCase.test(password)) return err[2]
-    if (!hasSymbol.test(password)) return err[3]
-    if (!hasMinLength.test(password)) return err[4]
-    return ''
-  }
-
-  async function handleSubmit(e: FormEvent<HTMLFormElement>) {
-    e.preventDefault()
-    setStatus(true)
-    const formData = new FormData(e.currentTarget)
-    const data = Object.fromEntries(formData)
-    setReturnError(validatePassword(data.password))
-    console.log(e.currentTarget.email.value)
-
-    const val = ValidateForm(
-      e.currentTarget.email.value,
-      e.currentTarget.firstName.value,
-      e.currentTarget.lastName.value,
-      e.currentTarget.DDD.value,
-      e.currentTarget.phoneNumber.value,
-      e.currentTarget.term_aceite.checked ? 1 : 0,
-    )
-
-    if (
-      data.password === data.password_confirmation &&
-      validatePassword(data.password) === '' &&
-      validarCPF(String(data.cpf)) &&
-      val === ''
-    ) {
-      const req = await InsertUser(data)
-      setReturnError(req.message)
-
-      if (req) {
-        setStatus(false)
-        alert('Cadastro feito com sucesso!')
-        router.push('/user/login')
-      }
-      if (req.message)
-        if (req.message.includes('The cpf has already been taken.')) {
-          setReturnError('The cpf has already been taken.')
-        } else if (req.message.includes('The email has already been taken.')) {
-          setReturnError('The email has already been taken.')
-        }
-    } else if (!validarCPF(String(data.cpf))) {
-      setReturnError('CPF invalido')
-    } else if (
-      data.password !== data.password_confirmation &&
-      data.password === '' &&
-      data.password_confirmation === ''
-    ) {
-      setReturnError('Senhas não correspondem')
-    } else if (val !== '') {
-      setReturnError(val)
-    }
-  }
+  const [state, action] = useFormState(InsertUser, {
+    ok: false,
+    error: '',
+    data: null,
+  })
 
   return (
     <div className="flex flex-col mx-auto justify-center h-[800px] w-full items-center">
@@ -98,11 +42,11 @@ export default function InsertUserPage() {
         Voltar
       </Link>
       <p className="text-xl mb-1 w-96 max-md:mb-0 max-md:w-80">Seu cadastro</p>
-      <form onSubmit={handleSubmit}>
+      <form action={action}>
         <ReCaptchaProvider
           reCaptchaKey={process.env.NEXT_PUBLIC_API_KEY_RECAPTCHA}
         >
-          {returnError === 'Preencha os dados!' && (
+          {state && state.error === 'Preencha os dados!' && (
             <span className="text-xs text-red-600">Preencha os dados!</span>
           )}
           <div className="flex flex-col mt-3 max-md:mt-3">
@@ -156,7 +100,7 @@ export default function InsertUserPage() {
               required
             />
           </div>
-          {returnError === 'Número de celular invalido.'}
+          {state && state.error === 'Número de celular invalido.'}
           <div className="flex flex-col mt-3">
             <label htmlFor="cpf">
               CPF: <span className="text-red-600">*</span>{' '}
@@ -170,10 +114,10 @@ export default function InsertUserPage() {
               required
             />
           </div>
-          {returnError === 'The cpf has already been taken.' && (
+          {state && state.error === 'The cpf has already been taken.' && (
             <span className="text-xs text-red-600">CPF já cadastrado</span>
           )}
-          {returnError === 'CPF invalido' && (
+          {state && state.error === 'CPF invalido' && (
             <span className="text-xs text-red-600">CPF inválido.</span>
           )}
           <div className="flex flex-col mt-3">
@@ -188,7 +132,7 @@ export default function InsertUserPage() {
               required
             />
           </div>
-          {returnError === 'The email has already been taken.' && (
+          {state && state.error === 'The email has already been taken.' && (
             <span className="text-xs text-red-600">E-mail já cadastrado</span>
           )}
           <div className="flex flex-col mt-3">
@@ -203,14 +147,6 @@ export default function InsertUserPage() {
               required
             />
           </div>
-          {err.map(
-            (erro, key) =>
-              returnError === erro && (
-                <span key={key} className="text-xs text-red-600">
-                  {returnError}
-                </span>
-              ),
-          )}
           <div className="flex flex-col mt-3">
             <label htmlFor="password_confirmation">
               Repetir senha: <span className="text-red-600">*</span>{' '}
@@ -229,13 +165,11 @@ export default function InsertUserPage() {
               Termos de aceitação <span className="text-red-600">*</span>
             </Link>
           </div>
+          {state && state.error && (
+            <span className="text-xs text-red-600">{state.error}</span>
+          )}
           <div className="flex justify-center">
-            <button
-              disabled={status}
-              className="mx-auto font-semibold w-52 h-10 bg-red-600 text-zinc-50 text-xl rounded-[9px] mt-3 max-md:w-44 max-md:mb-5 hover:bg-red-500"
-            >
-              Cadastrar
-            </button>
+            <FormButton />
           </div>
         </ReCaptchaProvider>
       </form>
