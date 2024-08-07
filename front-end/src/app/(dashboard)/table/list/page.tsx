8bfe58b396a15ddbace03a2009465a1fb898da06
andre <andrealbuquerque2001@gmail.com>
2024-08-07 12:04:07 -0300

    feat: update page

--- front-end/src/app/(dashboard)/table/list/page.tsx
+++ front-end/src/app/(dashboard)/table/list/page.tsx
@@ -1,11 +1,11 @@
-import GetAllTableService from '@/app/actions/table/getAllTable'
+import GetAllTableService from '@/actions/table/getAllTable'
+import { TableListComponent } from '@/components/tableList'
 import { TableInterface } from '@/data/type/table'
-import { ArrowLeft, Edit, PlusCircle, Trash2 } from 'lucide-react'
+import { ArrowLeft, PlusCircle } from 'lucide-react'
 import Link from 'next/link'
 
 export default async function GetAll() {
-  const dt = await GetAllTableService()
-  const data = dt.data
+  const data = (await GetAllTableService()) as TableInterface[]
 
   return (
     <div className="flex flex-col min-h-[90%] w-full px-3">
@@ -26,33 +26,7 @@ export default async function GetAll() {
       <p className="text-xl mb-1 w-96 max-md:mb-0 max-md:w-80 mt-5">
         Lista das mesas cadastradas
       </p>
-      <div className="space-y-5 mt-4">
-        {data.map((table: TableInterface, key: number) => (
-          <div className="" key={key}>
-            <div className="flex mx-auto justify-between">
-              <span className="max-w-96">Número: {table.numberMesa}</span>
-              <span className="max-md:hidden truncate w-96 text-center">
-                Capacidade: {table.lotacao}
-              </span>
-              <div className="flex items-center gap-3">
-                <Link
-                  href={`/table/update/${table.idMesa}`}
-                  title={`Editar o item, ${table.numberMesa}`}
-                >
-                  <Edit className="w-5 h-5" />
-                </Link>
-                <Link
-                  href={`/table/update/${table.idMesa}`}
-                  title={`Excluir o item, ${table.numberMesa}`}
-                >
-                  <Trash2 className="w-5 h-5" />
-                </Link>
-              </div>
-            </div>
-            <div className="w-full h-[1px] bg-zinc-600"></div>
-          </div>
-        ))}
-      </div>
+      <TableListComponent data={data} />
     </div>
   )
 }
