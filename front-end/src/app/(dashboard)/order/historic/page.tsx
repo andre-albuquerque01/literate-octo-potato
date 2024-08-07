e71c05d88b6f979b3818e061238944bccf0ae465
andre <andrealbuquerque2001@gmail.com>
2024-08-07 12:03:48 -0300

    feat: update page

--- front-end/src/app/(dashboard)/order/historic/page.tsx
+++ front-end/src/app/(dashboard)/order/historic/page.tsx
@@ -1,31 +1,10 @@
-import HistoricService from '@/app/actions/order/historic'
-import { MenuInterface } from '@/data/type/menu'
-import { ArrowLeft, EyeIcon } from 'lucide-react'
+import HistoricService from '@/actions/order/historic'
+import { GetAllMenuComponent } from '@/components/getAllMenu'
+import { ArrowLeft } from 'lucide-react'
 import Link from 'next/link'
 
 export default async function listMenu() {
-  const reqbody = await HistoricService()
-  const dt = await reqbody.json()
-  const data = dt.data.data
-
-  function formatarData(dataISO: string): string {
-    function padLeft(value: number): string {
-      return value < 10 ? '0' + value : value.toString()
-    }
-
-    if (dataISO) {
-      const data = new Date(dataISO)
-
-      const dia = padLeft(data.getDate())
-      const mes = padLeft(data.getMonth() + 1)
-      const ano = data.getFullYear()
-      const hora = padLeft(data.getHours())
-      const minuto = padLeft(data.getMinutes())
-
-      return `${hora}:${minuto} - ${dia}/${mes}/${ano}`
-    }
-    return ''
-  }
+  const data = await HistoricService()
 
   return (
     <div className="flex flex-col min-h-[90%] w-full px-3">
@@ -39,14 +18,14 @@ export default async function listMenu() {
       <p className="text-xl mb-1 w-96 max-md:mb-0 max-md:w-80 mt-5">
         Historico
       </p>
-      <div className="space-y-5 mt-4">
+      {/* <div className="space-y-5 mt-4">
         {data !== undefined && data.length > 0 ? (
           data.map((menu: MenuInterface, index: number) => (
             <div key={index}>
               <div className="flex mx-auto justify-between">
                 <span className="max-w-96">Codigo: {menu.codigo}</span>
                 <span className="max-md:hidden truncate w-96 text-center">
-                  Data: {formatarData(menu.updated_at)}
+                  Data: {FormatData(menu.updated_at)}
                 </span>
                 <div className="flex items-center gap-3">
                   <Link
@@ -63,7 +42,8 @@ export default async function listMenu() {
         ) : (
           <h1>Nenhuma pedido feito!</h1>
         )}
-      </div>
+      </div> */}
+      <GetAllMenuComponent data={data} />
     </div>
   )
 }
