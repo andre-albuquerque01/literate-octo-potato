fd5694db28af00299898de85f2860d94a1379499
andre <andrealbuquerque2001@gmail.com>
2024-08-09 12:02:10 -0300

    feat: update component

--- front-end/src/components/form-search.tsx
+++ front-end/src/components/form-search.tsx
@@ -37,7 +37,9 @@ export const FormSearch = () => {
       onSubmit={handleSearch}
       className="flex items-center gap-3 w-full rounded-xl px-5 py-3 border border-slate-400 md:mt-7 "
     >
-      <Search className="w-5 h-5" />
+      <button>
+        <Search className="w-5 h-5" />
+      </button>
       <input
         type="text"
         name="q"
