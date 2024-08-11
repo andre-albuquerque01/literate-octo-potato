0857fc929e03c316c44b56ec9b2f822b7ae60247
Andre <andrealbuquerque2001@gmail.com>
2024-08-11 18:00:00 -0300

    feat: create function

--- front-end/src/data/apiRoute.ts
+++ front-end/src/data/apiRoute.ts
@@ -1,6 +1,6 @@
 export default function ApiRoute(path: string, init?: RequestInit) {
   const baseUrl = 'http://localhost'
-  const apiPrefix = '/api/v1'
+  const apiPrefix = '/api/v2'
   const url = new URL(apiPrefix.concat(path), baseUrl)
 
   return fetch(url, init)
