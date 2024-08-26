d58784e5f63a82e69d23f176021a6f10ff802ec1
andre <andrealbuquerque2001@gmail.com>
2024-08-26 11:35:03 -0300

    feat: update action

--- front-end/src/actions/order/updateOrder.ts
+++ front-end/src/actions/order/updateOrder.ts
@@ -3,7 +3,6 @@
 import ApiRoute from '@/data/apiRoute'
 import { revalidateTag } from 'next/cache'
 import { cookies } from 'next/headers'
-import { NextResponse } from 'next/server'
 
 export async function UpdateOrder(reqBody: object, id: number) {
   try {
@@ -19,10 +18,8 @@ export async function UpdateOrder(reqBody: object, id: number) {
 
     const data = await response.json()
     revalidateTag('order')
-    return NextResponse.json({ data })
+    return data.data
   } catch (error) {
-    return new Response(JSON.stringify(error), {
-      status: 401,
-    })
+    return 'Error'
   }
 }
