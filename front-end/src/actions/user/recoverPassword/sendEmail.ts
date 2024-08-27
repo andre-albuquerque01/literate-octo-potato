722d3d32804a1a61c33a78059858f4b0a4c81f17
andre <andrealbuquerque2001@gmail.com>
2024-08-27 10:13:04 -0300

    feat: update action

--- front-end/src/actions/user/recoverPassword/sendEmail.ts
+++ front-end/src/actions/user/recoverPassword/sendEmail.ts
@@ -1,22 +1,38 @@
 'use server'
 
 import ApiRoute from '@/data/apiRoute'
+import ApiError from '@/data/function/apiErro'
 
-export async function SendEmail(requestBody: object) {
+export async function SendEmail(
+  state: { ok: boolean; error: string; data: null },
+  request: FormData,
+) {
+  const email = request.get('email') as string | null
+
   try {
+    if (!email) {
+      throw new Error('Preenchas os dados!')
+    }
     const response = await ApiRoute('/sendTokenRecover', {
       method: 'POST',
       headers: {
         Accept: 'application/json',
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify(requestBody),
+      body: request,
     })
 
     const data = await response.json()
 
-    return data
+    const message =
+      typeof data.message === 'string'
+        ? data.message
+        : JSON.stringify(data.message)
+
+    if (message && message.includes('Failed to send email'))
+      throw new Error('E-mail não cadastrado!')
+    return { data: null, error: '', ok: true }
   } catch (error) {
-    return 'Houve error'
+    return ApiError(error)
   }
 }
