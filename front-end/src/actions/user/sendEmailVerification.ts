08f56fbc25b98236f45e9b55931772ef6f181e3d
andre <andrealbuquerque2001@gmail.com>
2024-08-27 10:12:44 -0300

    feat: update action

--- front-end/src/actions/user/sendEmailVerification.ts
+++ front-end/src/actions/user/sendEmailVerification.ts
@@ -1,25 +1,39 @@
 'use server'
 
 import ApiRoute from '@/data/apiRoute'
+import ApiError from '@/data/function/apiErro'
 
-export async function SendEmailVerification(requestBody: { email: string }) {
+export async function SendEmailVerification(
+  state: { ok: boolean; error: string; data: null },
+  request: FormData,
+) {
+  const email = request.get('email') as string | null
+
   try {
-    if (!requestBody.email) {
-      return { message: 'E-mail invalido' }
+    if (!email) {
+      throw new Error('Preenchas os dados!')
     }
+
     const response = await ApiRoute('/reSendEmail', {
       method: 'POST',
       headers: {
         Accept: 'application/json',
-        'Content-Type': 'application/json',
       },
-      body: JSON.stringify(requestBody),
+      body: request,
     })
 
     const data = await response.json()
 
-    return data
+    const message =
+      typeof data.message === 'string'
+        ? data.message
+        : JSON.stringify(data.message)
+
+    if (message && message.includes('E-mail não cadastrado.'))
+      throw new Error('E-mail não cadastrado.')
+
+    return { data: null, error: '', ok: true }
   } catch (error) {
-    return 'Houve error'
+    return ApiError(error)
   }
 }
