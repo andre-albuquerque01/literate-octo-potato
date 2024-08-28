2920a73147b5ac464a235217132b1eddb1741e20
Andre <andrealbuquerque2001@gmail.com>
2024-08-28 17:00:00 -0300

    feat: update migrate

--- back-end/database/migrations/2024_02_11_100917_create_categories_table.php
+++ back-end/database/migrations/2024_02_11_100917_create_categories_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('categories', function (Blueprint $table) {
-            $table->id('idCategory');
+            $table->ulid('idCategory')->primary();
             $table->string('typeCategory');
             $table->string('urlImageCategory')->nullable();
             $table->timestamps();
