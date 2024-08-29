a9df178a94009ec771169693c0bb529cf6050938
andre <andrealbuquerque2001@gmail.com>
2024-08-29 12:13:16 -0300

    feat: update migrate

--- back-end/database/migrations/2024_02_11_100917_create_categories_table.php
+++ back-end/database/migrations/2024_02_11_100917_create_categories_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->ulid('idCategory')->primary();
             $table->string('typeCategory');
             $table->string('urlImageCategory')->nullable();
+            $table->softDeletes();
             $table->timestamps();
         });
     }
