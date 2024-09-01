4536ed8995d84396e5e377a9e008acd26c1dc493
andre <andrealbuquerque2001@gmail.com>
2024-09-01 17:19:33 -0300

    feat: update action

--- front-end/src/actions/order/updateOrder.ts
+++ front-end/src/actions/order/updateOrder.ts
@@ -1,10 +1,12 @@
 'use server'
 
 import ApiRoute from '@/data/apiRoute'
+import { TranslateErroOrder } from '@/data/function/translate/translateErroOrder'
+import { ErrorResponse } from '@/data/type/erros'
 import { revalidateTag } from 'next/cache'
 import { cookies } from 'next/headers'
 
-export async function UpdateOrder(reqBody: object, id: number) {
+export async function UpdateOrder(reqBody: object, id: string) {
   try {
     const response = await ApiRoute(`/order/${id}`, {
       method: 'PUT',
@@ -16,10 +18,22 @@ export async function UpdateOrder(reqBody: object, id: number) {
       body: JSON.stringify(reqBody),
     })
 
-    const data = await response.json()
+    const data = (await response.json()) as ErrorResponse
+
+    if (!response.ok) {
+      const errorsArray = Object.values(data.errors ?? {})
+        .flat()
+        .map(TranslateErroOrder)
+
+      const formattedErrors = errorsArray
+        .map((error) => `- ${error}`)
+        .join('\n')
+
+      return `Por favor, verifique os seguintes campos:\n${formattedErrors}`
+    }
     revalidateTag('order')
-    return data.data
+    return 'success'
   } catch (error) {
-    return 'Error'
+    return 'Desculpe, ocorreu um erro ao alterar o item. Por favor, tente novamente mais tarde.'
   }
 }
