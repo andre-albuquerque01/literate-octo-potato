3e17ecf2952e02c6b3d0219540749de639faa0ab
andre <andrealbuquerque2001@gmail.com>
2024-09-01 17:19:45 -0300

    feat: update action

--- front-end/src/actions/table/updateTable.ts
+++ front-end/src/actions/table/updateTable.ts
@@ -1,10 +1,12 @@
 'use server'
 
 import ApiRoute from '@/data/apiRoute'
+import { TranslateErroTable } from '@/data/function/translate/translateErroTable'
+import { ErrorResponse } from '@/data/type/erros'
 import { revalidateTag } from 'next/cache'
 import { cookies } from 'next/headers'
 
-export async function UpdateTable(reqBody: object, id: number) {
+export async function UpdateTable(reqBody: object, id: string) {
   try {
     const response = await ApiRoute(`/table/${id}`, {
       method: 'PUT',
@@ -15,11 +17,22 @@ export async function UpdateTable(reqBody: object, id: number) {
       },
       body: JSON.stringify(reqBody),
     })
-    const data = await response.json()
+    const data = (await response.json()) as ErrorResponse
 
+    if (!response.ok) {
+      const errorsArray = Object.values(data.errors ?? {})
+        .flat()
+        .map(TranslateErroTable)
+
+      const formattedErrors = errorsArray
+        .map((error) => `- ${error}`)
+        .join('\n')
+
+      return `Por favor, verifique os seguintes campos:\n${formattedErrors}`
+    }
     revalidateTag('table')
-    return data.data.message
+    return 'success'
   } catch (error) {
-    return 'Error'
+    return 'Desculpe, ocorreu um erro ao alterar o item. Por favor, tente novamente mais tarde.'
   }
 }
