da7ae2650d96fee1375c897a9fb216baa8b37186
andre <andrealbuquerque2001@gmail.com>
2024-09-03 12:34:48 -0300

    feat: update page

--- front-end/src/app/(dashboard)/dashboard/page.tsx
+++ front-end/src/app/(dashboard)/dashboard/page.tsx
@@ -1,16 +1,15 @@
 import { GetAllItens } from '@/actions/itens/getAllItens'
-import { SnackCarrossel } from '@/components/SnackCarousel'
-import { Carrossel } from '@/components/carousel'
-import { DinnerCarrossel } from '@/components/dinnerCarousel'
-import { DrinksCarrossel } from '@/components/drinksCarousel'
-import { LunchCarrossel } from '@/components/lunchCarousel'
+import { SnackCarrossel } from '@/components/itens/SnackCarousel'
+import { Carrossel } from '@/components/itens/carousel'
+import { DinnerCarrossel } from '@/components/itens/dinnerCarousel'
+import { DrinksCarrossel } from '@/components/itens/drinksCarousel'
+import { LunchCarrossel } from '@/components/itens/lunchCarousel'
 
 export default async function Dashboard() {
-  const reqBody = await GetAllItens()
-  const data = reqBody.data
+  const data = await GetAllItens()
 
   return (
-    <div>
+    <div className="max-md:mb-14 mb-10">
       <div>
         <Carrossel data={data} />
       </div>
