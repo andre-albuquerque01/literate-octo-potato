7bfbc54e20f52c9fa30506ac543d3786fd54c493
andre <andrealbuquerque2001@gmail.com>
2024-09-03 12:34:19 -0300

    feat: update action

--- front-end/src/actions/user/updatePassword.ts
+++ front-end/src/actions/user/updatePassword.ts
@@ -2,61 +2,74 @@
 
 import ApiRoute from '@/data/apiRoute'
 import VerificationPasswordWithReturn from '@/data/function/validatePasswordWithReturn'
+import { ApiErrorResponse } from '@/data/type/errors'
 import { cookies } from 'next/headers'
 
 export async function UpdatePasswordUser(request: FormData) {
   const password = request.get('password') as string
+  const passwordNew = request.get('password_new') as string
   const passwordConfirmation = request.get('password_confirmation') as
     | string
     | null
   try {
-    if (password !== passwordConfirmation) {
+    if (!password || passwordNew || passwordConfirmation) {
+      return 'Preenchas os dados!'
+    }
+    if (passwordNew !== passwordConfirmation) {
       return 'Senha incompativel!'
     }
-    VerificationPasswordWithReturn(password)
+    VerificationPasswordWithReturn(passwordNew)
 
+    const reqBody = Object.fromEntries(request)
     const response = await ApiRoute('/userPassword', {
       method: 'PUT',
       headers: {
         Accept: 'application/json',
+        'Content-type': 'application/json',
         Authorization: `Bearer ${cookies().get('token')?.value}`,
       },
-      body: request,
+      body: JSON.stringify(reqBody),
     })
 
-    const data = await response.json()
+    const data: ApiErrorResponse = await response.json()
+    console.log(data)
 
-    const message =
-      data && data.data && typeof data.data.message === 'string'
-        ? data.data.message
-        : JSON.stringify(data?.data?.message || '')
+    if (data.errors) {
+      const errors = data.errors
 
-    if (message && message.includes('The email has already been taken.'))
-      return 'E-mail já cadastrado!'
-    if (
-      message &&
-      message.includes('The password field must be at least 8 characters.')
-    )
-      return 'A senha deve ter ao menos 8 caracters'
-    if (
-      message &&
-      message.includes('The password field must contain at least one symbol.')
-    )
-      return 'A senha precisa de um caracter especial'
-    if (
-      message &&
-      message.includes(
-        'The password field must contain at least one uppercase and one lowercase letter.',
-      )
-    )
-      return 'Senha precisa de ao menos uma letra maiúscula e uma minúsculas'
-    if (
-      message &&
-      message.includes(
-        'The given password has appeared in a data leak. Please choose a different password.',
-      )
-    )
-      return 'Senha fraca.'
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      for (const [field, messages] of Object.entries(errors)) {
+        messages.forEach((message) => {
+          if (message === 'The password field is required.') {
+            return 'O campo senha é obrigatório.'
+          } else if (
+            message === 'The password confirmation field is required.'
+          ) {
+            return 'O campo repita senha é obrigatório.'
+          } else if (
+            message === 'The password field must be at least 8 characters.'
+          ) {
+            return 'A senha deve ter ao menos 8 caracteres.'
+          } else if (
+            message === 'The password field must contain at least one symbol.'
+          ) {
+            return 'A senha precisa de um caracter especial.'
+          } else if (
+            message ===
+            'The password field must contain at least one uppercase and one lowercase letter.'
+          ) {
+            return 'Senha precisa de ao menos uma letra maiúscula e uma minúsculas.'
+          } else if (
+            message ===
+            'The given password has appeared in a data leak. Please choose a different password.'
+          ) {
+            return 'Senha fraca.'
+          } else if (message === 'The password field is required.') {
+            return 'O campo senha é obrigatório.'
+          }
+        })
+      }
+    }
 
     return 'success'
   } catch (error) {
