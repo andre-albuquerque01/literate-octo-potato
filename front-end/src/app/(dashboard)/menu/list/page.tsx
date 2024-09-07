01f7af4ce8f6d373ef2947e0585f5d83d9b7ab52
andre <andrealbuquerque2001@gmail.com>
2024-09-07 11:04:15 -0300

    feat: update page

--- front-end/src/app/(dashboard)/menu/list/page.tsx
+++ front-end/src/app/(dashboard)/menu/list/page.tsx
@@ -1,16 +1,38 @@
 import GetAllMenuService from '@/actions/menu/getAllMenu'
-import { GetAllMenuComponent } from '@/components/getAllMenu'
+import { GetCpfOpenMenu } from '@/actions/menu/getCpfOpenMenu'
+import { FormSearchMenuCPF } from '@/components/forms/form-search-menuCpf'
+import { GetAllMenuComponent } from '@/components/menu/getAllMenu'
 import { ArrowLeft, PlusCircle } from 'lucide-react'
 import Link from 'next/link'
 
-export default async function listMenu() {
-  const data = await GetAllMenuService()
+interface PropsSearchParams {
+  searchParams: {
+    page: number
+    q: string
+  }
+}
+
+export default async function listMenu({ searchParams }: PropsSearchParams) {
+  const { q: queryQ } = searchParams
+
+  let { page } = searchParams || 1
+  if (page === undefined) page = 1
+
+  let data = []
+
+  if (queryQ && queryQ !== '') {
+    const dt = await GetCpfOpenMenu(queryQ)
+    data = dt
+  } else {
+    const dt = await GetAllMenuService()
+    data = dt
+  }
 
   return (
     <div className="flex flex-col min-h-[90%] w-full px-3">
       <Link
         href="/user"
-        className="md:hidden flex items-center gap-1 text-sm mb-3 w-96 max-md:mt-3 max-md:w-80"
+        className="flex items-center gap-1 text-sm w-96 mt-5 max-md:w-80"
       >
         <ArrowLeft className="w-5 h-5" />
         Voltar
@@ -25,6 +47,7 @@ export default async function listMenu() {
       <p className="text-xl mb-1 w-96 max-md:mb-0 max-md:w-80 mt-5">
         Lista de comandas abertas
       </p>
+      <FormSearchMenuCPF />
       <GetAllMenuComponent data={data} />
     </div>
   )
