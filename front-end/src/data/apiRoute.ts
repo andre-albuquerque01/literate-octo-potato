8b31db2afdbc222fdc90c6e139dcd8598a0c6fad
andre <andrealbuquerque2001@gmail.com>
2024-09-08 19:37:31 -0300

    feat: update function

--- front-end/src/data/apiRoute.ts
+++ front-end/src/data/apiRoute.ts
@@ -1,5 +1,5 @@
 export default function ApiRoute(path: string, init?: RequestInit) {
-  const baseUrl = 'http://localhost'
+  const baseUrl = process.env.NEXT_PUBLIC_ROUTE_API
   const apiPrefix = '/api/v2'
   const url = new URL(apiPrefix.concat(path), baseUrl)
 
