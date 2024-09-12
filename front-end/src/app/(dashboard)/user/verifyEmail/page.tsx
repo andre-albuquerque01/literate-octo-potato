476180016d9bb0eb664522b4c832c16bc19efbec
andre <andrealbuquerque2001@gmail.com>
2024-09-12 13:03:35 -0300

    feat: update page

--- front-end/src/app/(dashboard)/user/verifyEmail/page.tsx
+++ front-end/src/app/(dashboard)/user/verifyEmail/page.tsx
@@ -1,43 +1,56 @@
 'use client'
+
 import { SendEmailVerification } from '@/actions/user/sendEmailVerification'
 import { ArrowLeft } from 'lucide-react'
-import { ReCaptchaProvider } from 'next-recaptcha-v3'
 import Link from 'next/link'
 import { useRouter } from 'next/navigation'
-import { FormEvent, useState } from 'react'
+import { useEffect } from 'react'
+import { useFormState, useFormStatus } from 'react-dom'
+
+const BtnForm = () => {
+  const { pending } = useFormStatus()
+  return (
+    <>
+      {pending ? (
+        <div className="flex justify-center">
+          <button
+            className="mx-auto font-semibold w-52 h-10 bg-red-600 text-zinc-50 text-xl rounded-[9px] mt-3 max-md:w-44 max-md:mb-5 hover:bg-red-500"
+            disabled={pending}
+          >
+            Enviando...
+          </button>
+        </div>
+      ) : (
+        <div className="flex justify-center">
+          <button className="mx-auto font-semibold w-52 h-10 bg-red-600 text-zinc-50 text-xl rounded-[9px] mt-3 max-md:w-44 max-md:mb-5 hover:bg-red-500">
+            Enviar
+          </button>
+        </div>
+      )}
+    </>
+  )
+}
 
 export default function VerifyEmaiPage() {
-  const [error, setError] = useState<string>('')
-  const [status, setStatus] = useState(false)
-  const router = useRouter()
-  async function handleSubmit(e: FormEvent<HTMLFormElement>) {
-    e.preventDefault()
-    setStatus(true)
-    if (e.currentTarget.email.value !== '') {
-      const req = await SendEmailVerification({
-        email: e.currentTarget.email.value,
-      })
-      if (req.message === 'sucess') {
-        setStatus(false)
-        alert('E-mail enviado')
-        router.back()
-      } else if (req.message === 'E-mail não cadastrado.') {
-        setStatus(false)
-        setError('E-mail não cadastrado.')
-      } else {
-        setStatus(false)
-        setError('E-mail não encontrado')
-      }
-    } else {
-      setError('Preencha o campo e-mail')
-    }
-  }
+  const [state, action] = useFormState(SendEmailVerification, {
+    ok: false,
+    error: '',
+    data: null,
+  })
 
+  const router = useRouter()
+  useEffect(() => {
+    if (state && state.ok) {
+      alert('Email enviado!')
+      router.push('/user/login')
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [state])
   return (
-    <div className="flex flex-col md:h-[800px] justify-center max-md:h-[800px] items-center">
+    <div className="flex flex-col mx-auto justify-center h-screen mt-[-100px] w-full items-center">
       <Link
         href="/user/login"
-        className="md:hidden flex items-center gap-1 text-sm mb-10 w-96 max-md:w-80"
+        className="flex items-center gap-1 text-sm w-96 max-md:w-80 mb-5"
       >
         <ArrowLeft className="w-5 h-5" />
         Voltar
@@ -51,32 +64,23 @@ export default function VerifyEmaiPage() {
         entrada e também a pasta de spam, caso não encontre o email em sua caixa
         principal.
       </p>
-      <form onSubmit={handleSubmit} className="w-96 max-md:w-80">
-        <ReCaptchaProvider
-          reCaptchaKey={process.env.NEXT_PUBLIC_API_KEY_RECAPTCHA}
-        >
-          <div className="flex flex-col mt-3">
-            <label htmlFor="email">
-              E-mail: <span className="text-red-600">*</span>{' '}
-            </label>
-            <input
-              type="email"
-              name="email"
-              id="email"
-              className="w-full h-9 border border-zinc-400 rounded-[5px] px-2"
-              required
-            />
-          </div>
-          <span className="text-red-600 text-xs">{error}</span>
-          <div className="flex justify-center">
-            <button
-              disabled={status}
-              className="mx-auto font-semibold w-52 h-10 bg-red-600 text-zinc-50 text-xl rounded-[9px] mt-3 max-md:w-44 max-md:mb-5 hover:bg-red-500"
-            >
-              Enviar
-            </button>
-          </div>
-        </ReCaptchaProvider>
+      <form action={action} className="w-96 max-md:w-80">
+        <div className="flex flex-col mt-3">
+          <label htmlFor="email">
+            E-mail: <span className="text-red-600">*</span>{' '}
+          </label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            className="w-full h-9 border border-zinc-400 rounded-[5px] px-2"
+            required
+          />
+        </div>
+        <span className="text-red-600 text-xs">
+          {state.error && state.error}
+        </span>
+        <BtnForm />
       </form>
     </div>
   )
