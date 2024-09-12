977de817a4348122d02ecb41da127d77644f1d72
andre <andrealbuquerque2001@gmail.com>
2024-09-12 13:03:50 -0300

    feat: update page

--- front-end/src/app/(dashboard)/user/update/page.tsx
+++ front-end/src/app/(dashboard)/user/update/page.tsx
@@ -1,71 +1,75 @@
 'use client'
 import { ShowUser } from '@/actions/user/showUser'
 import { UpdateUser } from '@/actions/user/updateUser'
-import { BtnForm } from '@/components/btnForm'
-import { ValidateForm } from '@/data/function/validateForm'
 import { UserInterface } from '@/data/type/user'
 import { ArrowLeft } from 'lucide-react'
-import { ReCaptchaProvider } from 'next-recaptcha-v3'
 import Link from 'next/link'
 import { useRouter } from 'next/navigation'
-import { FormEvent, useEffect, useState } from 'react'
+import { FormEvent, Suspense, useEffect, useState } from 'react'
+
+const BtnForm = ({ pending }: { pending: boolean }) => {
+  return (
+    <>
+      {pending ? (
+        <div className="flex justify-center">
+          <button
+            className="mx-auto font-semibold w-52 h-10 bg-red-600 text-zinc-50 text-xl rounded-[9px] mt-3 max-md:w-44 max-md:mb-5 hover:bg-red-500"
+            disabled={pending}
+          >
+            Alterando...
+          </button>
+        </div>
+      ) : (
+        <div className="flex justify-center">
+          <button className="mx-auto font-semibold w-52 h-10 bg-red-600 text-zinc-50 text-xl rounded-[9px] mt-3 max-md:w-44 max-md:mb-5 hover:bg-red-500">
+            Alterar
+          </button>
+        </div>
+      )}
+    </>
+  )
+}
 
 export default function UpdateUserPage() {
   const [returnError, setReturnError] = useState<string>('')
   const [data, setData] = useState<UserInterface>()
+  const [status, setStatus] = useState<boolean>(false)
   const router = useRouter()
-
   useEffect(() => {
     const get = async () => {
-      const reqBody = await ShowUser()
-      const date = reqBody.data
-      setData(date)
+      const dt = await ShowUser()
+      setData(dt)
     }
     get()
   }, [])
 
   async function handleSubmit(e: FormEvent<HTMLFormElement>) {
     e.preventDefault()
-
+    setStatus(true)
     const formData = new FormData(e.currentTarget)
-    const data = Object.fromEntries(formData)
-    const req = await UpdateUser(data)
-    setReturnError(req.message)
-
-    const val = ValidateForm(
-      e.currentTarget.email.value,
-      e.currentTarget.firstName.value,
-      e.currentTarget.lastName.value,
-      e.currentTarget.DDD.value,
-      e.currentTarget.phoneNumber.value,
-    )
-
-    if (req.message) {
-      if (req.message.includes('The cpf has already been taken.')) {
-        setReturnError('The cpf has already been taken.')
-      } else if (req.message === 'sucess') {
-        alert('Cadastro alterado com sucesso!')
-        router.back()
-      }
-    } else if (val !== '') {
-      setReturnError(val)
+    const req = await UpdateUser(formData)
+    if (req === '') {
+      alert('Alterado com sucesso.')
+      router.back()
     }
+    setReturnError(req)
+    setStatus(false)
   }
 
   return (
-    <div className="flex flex-col mx-auto justify-center h-[90%] w-full items-center">
-      <Link
-        href="/user"
-        className="md:hidden flex items-center gap-1 text-sm mb-3 w-96 max-md:mt-24 max-md:w-80"
-      >
-        <ArrowLeft className="w-5 h-5" />
-        Voltar
-      </Link>
-      <p className="text-xl mb-1 w-96 max-md:mb-0 max-md:w-80">Seu cadastro</p>
-      <form onSubmit={handleSubmit}>
-        <ReCaptchaProvider
-          reCaptchaKey={process.env.NEXT_PUBLIC_API_KEY_RECAPTCHA}
+    <Suspense>
+      <div className="flex flex-col mx-auto justify-center h-screen md:mt-[-115px] w-full items-center">
+        <Link
+          href="/user"
+          className="flex items-center gap-1 text-sm w-96 mt-3 md:mt-10 max-md:w-80 mb-5"
         >
+          <ArrowLeft className="w-5 h-5" />
+          Voltar
+        </Link>
+        <p className="text-xl mb-1 w-96 max-md:mb-0 max-md:w-80">
+          Alterar seus dados
+        </p>
+        <form onSubmit={handleSubmit}>
           <div className="flex flex-col mt-3 max-md:mt-3">
             <label htmlFor="firstName">
               Primeiro nome: <span className="text-red-600">*</span>{' '}
@@ -100,8 +104,6 @@ export default function UpdateUserPage() {
               type="number"
               name="DDD"
               id="DDD"
-              min="0"
-              maxLength={999}
               className="w-96 h-9 border border-zinc-400 rounded-[5px] max-md:w-80 px-2"
               defaultValue={data?.DDD ?? ''}
               required
@@ -112,10 +114,9 @@ export default function UpdateUserPage() {
               Número de telefone: <span className="text-red-600">*</span>{' '}
             </label>
             <input
-              type="number"
+              type="text"
               name="phoneNumber"
               id="phoneNumber"
-              min={9999}
               className="w-96 h-9 border border-zinc-400 rounded-[5px] max-md:w-80 px-2"
               defaultValue={data?.phoneNumber ?? ''}
               required
@@ -126,12 +127,9 @@ export default function UpdateUserPage() {
               CPF: <span className="text-red-600">*</span>{' '}
             </label>
             <input
-              type="number"
-              name="cpf"
-              id="cpf"
-              min={9999}
-              className="w-96 h-9 border border-zinc-400 rounded-[5px] max-md:w-80 px-2"
-              defaultValue={data?.cpf ?? ''}
+              type="text"
+              className="w-96 h-9 border bg-zinc-400 rounded-[5px] max-md:w-80 px-2"
+              defaultValue={data?.cpf}
               readOnly
               required
             />
@@ -145,10 +143,8 @@ export default function UpdateUserPage() {
             </label>
             <input
               type="email"
-              name="email"
-              id="email"
-              className="w-96 h-9 border border-zinc-400 rounded-[5px] max-md:w-80 px-2"
-              defaultValue={data?.email ?? ''}
+              className="w-96 h-9 border bg-zinc-400 rounded-[5px] max-md:w-80 px-2"
+              defaultValue={data?.email}
               readOnly
               required
             />
@@ -165,9 +161,9 @@ export default function UpdateUserPage() {
               required
             />
           </div>
-          <BtnForm title="Alterar" />
-        </ReCaptchaProvider>
-      </form>
-    </div>
+          <BtnForm pending={status} />
+        </form>
+      </div>
+    </Suspense>
   )
 }
