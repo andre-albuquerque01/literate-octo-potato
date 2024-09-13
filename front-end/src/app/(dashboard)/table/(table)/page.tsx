dce4903f0c3e9426b3ba105c0dd1d7aae769d9d6
andre <andrealbuquerque2001@gmail.com>
2024-09-13 14:41:07 -0300

    feat: update page

--- front-end/src/app/(dashboard)/table/(table)/page.tsx
+++ front-end/src/app/(dashboard)/table/(table)/page.tsx
@@ -1,13 +1,20 @@
 import GetAllTableService from '@/actions/table/getAllTable'
-import { TableStatus } from '@/components/Table-status'
-import { ArrowRight } from 'lucide-react'
+import { TableStatus } from '@/components/itens/Table-status'
+import { ArrowLeft, ArrowRight } from 'lucide-react'
 import Link from 'next/link'
 
 export default async function Tables() {
   const data = await GetAllTableService()
 
   return (
-    <div className="max-md:w-[80%] max-md:mx-auto md:mt-4">
+    <div className="max-md:mx-auto md:mt-4">
+      <Link
+        href="/user"
+        className="flex items-center gap-1 text-sm w-96 mt-3 max-md:w-80"
+      >
+        <ArrowLeft className="w-5 h-5" />
+        Voltar
+      </Link>
       <Link
         href="/table/list"
         className="flex items-center gap-2 w-[170px] py-2 text-sm max-md:mb-0 max-md:w-80 mt-3 border-b-2 border-transparent hover:border-red-600 transition duration-500"
