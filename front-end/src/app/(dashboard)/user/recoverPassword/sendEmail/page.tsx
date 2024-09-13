b4b0ec8cc521a0537642849f121277896870fa9e
andre <andrealbuquerque2001@gmail.com>
2024-09-13 14:40:15 -0300

    feat: update page

--- front-end/src/app/(dashboard)/user/recoverPassword/sendEmail/page.tsx
+++ front-end/src/app/(dashboard)/user/recoverPassword/sendEmail/page.tsx
@@ -1,35 +1,52 @@
 'use client'
 import { SendEmail } from '@/actions/user/recoverPassword/sendEmail'
+import { BtnForm } from '@/components/buttons/btnForm'
 import { ArrowLeft } from 'lucide-react'
 import Link from 'next/link'
 import { useRouter } from 'next/navigation'
-import { FormEvent, useState } from 'react'
+import { useEffect } from 'react'
+import { useFormState, useFormStatus } from 'react-dom'
 
-export default function SendEmailPage() {
-  const [error, setError] = useState<string>('')
-  const [status, setStatus] = useState(false)
-  const router = useRouter()
-  async function handleSubmit(e: FormEvent<HTMLFormElement>) {
-    e.preventDefault()
-    setStatus(true)
-    const formData = new FormData(e.currentTarget)
-    const data = Object.fromEntries(formData)
-    const req = await SendEmail(data)
-
-    if (req.message === 'send e-mail') {
-      setStatus(false)
-      alert('E-mail enviado')
-      router.push('/user/recoverPassword/validationToken')
-    } else {
-      setError('E-mail não encontrado')
-    }
-  }
+function FormButton() {
+  const { pending } = useFormStatus()
 
   return (
-    <div className="flex flex-col md:h-[800px] justify-center max-md:h-[800px] items-center">
+    <>
+      {pending ? (
+        <button
+          className="mx-auto font-semibold w-52 h-10 bg-red-600 text-zinc-50 text-xl rounded-[9px] mt-3 max-md:w-44 max-md:mb-5 hover:bg-red-500"
+          disabled={pending}
+        >
+          Enviando...
+        </button>
+      ) : (
+        <BtnForm title="Enviar" />
+      )}
+    </>
+  )
+}
+
+export default function SendEmailPage() {
+  const [state, action] = useFormState(SendEmail, {
+    ok: false,
+    error: '',
+    data: null,
+  })
+
+  const router = useRouter()
+  useEffect(() => {
+    if (state && state.ok) {
+      alert('Email enviado!')
+      router.push('/user/login')
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [state])
+
+  return (
+    <div className="flex flex-col mx-auto justify-center h-screen mt-[-100px] w-full items-center">
       <Link
         href="/user/login"
-        className="md:hidden flex items-center gap-1 text-sm mb-10 w-96 max-md:w-80"
+        className="flex items-center gap-1 text-sm w-96 max-md:w-80 mb-5"
       >
         <ArrowLeft className="w-5 h-5" />
         Voltar
@@ -41,7 +58,7 @@ export default function SendEmailPage() {
         entrada e também a pasta de spam, caso não encontre o email em sua caixa
         principal.
       </p>
-      <form onSubmit={handleSubmit} className="w-96 max-md:w-80">
+      <form action={action} className="w-96 max-md:w-80">
         <div className="flex flex-col mt-3">
           <label htmlFor="email">
             E-mail: <span className="text-red-600">*</span>{' '}
@@ -54,14 +71,11 @@ export default function SendEmailPage() {
             required
           />
         </div>
-        <span className="text-red-600 text-xs">{error}</span>
+        <span className="text-red-600 text-xs">
+          {state.error && state.error}
+        </span>
         <div className="flex justify-center">
-          <button
-            disabled={status}
-            className="mx-auto font-semibold w-52 h-10 bg-red-600 text-zinc-50 text-xl rounded-[9px] mt-3 max-md:w-44 max-md:mb-5 hover:bg-red-500"
-          >
-            Enviar
-          </button>
+          <FormButton />
         </div>
       </form>
     </div>
