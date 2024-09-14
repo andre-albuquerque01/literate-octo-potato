a0999a73976aeebefde65aef7d3caf942702e252
andre <andrealbuquerque2001@gmail.com>
2024-09-14 10:19:13 -0300

    feat: update layout

---
+++ front-end/src/app/(dashboard)/user/recoverPassword/[token]/layout.tsx
@@ -0,0 +1,9 @@
+import { Metadata } from 'next'
+import { ReactNode } from 'react'
+
+export const metadata: Metadata = {
+  title: 'Alterar senha',
+}
+export default function Layout({ children }: { children: ReactNode }) {
+  return <div className="">{children}</div>
+}
