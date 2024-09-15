5287eb2371d3c48af454754fd7b19af319861b5b
andre <andrealbuquerque2001@gmail.com>
2024-09-15 17:02:19 -0300

    feat: new layout

---
+++ front-end/src/app/(dashboard)/order/user/menuUser/layout.tsx
@@ -0,0 +1,10 @@
+import { cookies } from 'next/headers'
+import { redirect } from 'next/navigation'
+import { ReactNode } from 'react'
+
+export default function Layout({ children }: { children: ReactNode }) {
+  const cookiesStore = cookies()
+  const token = cookiesStore.has('token')
+  if (!token) redirect('/user/login')
+  return <div>{children}</div>
+}
