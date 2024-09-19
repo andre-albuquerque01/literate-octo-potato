3c80cf793df68fcc7f3830c7d8b2b76721b6ec31
andre <andrealbuquerque2001@gmail.com>
2024-09-19 12:00:14 -0300

    feat: update page

--- front-end/src/app/(dashboard)/category/list/page.tsx
+++ front-end/src/app/(dashboard)/category/list/page.tsx
@@ -6,10 +6,10 @@ import Link from 'next/link'
 export default async function GetAll() {
   const data = await GetAllCategory()
   return (
-    <div className="flex flex-col min-h-[90%] w-full px-3">
+    <div className="flex flex-col w-full px-3">
       <Link
         href="/user"
-        className="md:hidden flex items-center gap-1 text-sm mb-3 w-96 max-md:mt-3 max-md:w-80"
+        className="flex items-center gap-1 text-sm mb-3 w-20 mt-3"
       >
         <ArrowLeft className="w-5 h-5" />
         Voltar
