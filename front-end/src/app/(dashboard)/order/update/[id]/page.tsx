399ecc9b2589440cc89027344d1b25da54c18ee4
andre <andrealbuquerque2001@gmail.com>
2024-09-26 11:08:53 -0300

    feat: update page

--- front-end/src/app/(dashboard)/order/update/[id]/page.tsx
+++ front-end/src/app/(dashboard)/order/update/[id]/page.tsx
@@ -2,17 +2,39 @@
 import { GetIdItens } from '@/actions/itens/getIdItens'
 import GetOrderService from '@/actions/order/getOrder'
 import { UpdateOrder } from '@/actions/order/updateOrder'
-import { BtnForm } from '@/components/btnForm'
 import { InterfaceItens } from '@/data/type/interfaceItens'
 import { ArrowLeft } from 'lucide-react'
 import Link from 'next/link'
 import { useRouter, useSearchParams } from 'next/navigation'
 import { FormEvent, useEffect, useState } from 'react'
 
+const BtnForm = ({ pending }: { pending: boolean }) => {
+  return (
+    <>
+      {pending ? (
+        <div className="flex justify-center">
+          <button
+            className="mx-auto font-semibold w-52 h-10 bg-red-600 text-zinc-50 text-xl rounded-[9px] mt-3 max-md:w-44 max-md:mb-5 hover:bg-red-500"
+            disabled={pending}
+          >
+            Alterando...
+          </button>
+        </div>
+      ) : (
+        <div className="flex justify-center">
+          <button className="mx-auto font-semibold w-52 h-10 bg-red-600 text-zinc-50 text-xl rounded-[9px] mt-3 max-md:w-44 max-md:mb-5 hover:bg-red-500">
+            Alterar
+          </button>
+        </div>
+      )}
+    </>
+  )
+}
+
 export default function UpdateOrderPage({
   params,
 }: {
-  params: { id: number }
+  params: { id: string }
 }) {
   const router = useRouter()
   const searchParams = useSearchParams()
@@ -20,11 +42,13 @@ export default function UpdateOrderPage({
   const itens = searchParams.get('iten')
   const menu = searchParams.get('menu')
 
+  const [status, setStatus] = useState<boolean>(false)
   const [data, setData] = useState<InterfaceItens>()
   const [dados, setDados] = useState({
     desconto: '',
     qtdOrder: '',
     tip: '',
+    observation: '',
     valueOrder: 0,
     idItens: itens,
     idMenu: menu,
@@ -65,42 +89,47 @@ export default function UpdateOrderPage({
 
   useEffect(() => {
     const handleData = async () => {
-      const reqbody = await GetOrderService(params.id)
-      const dat = reqbody.data
-      setDados(dat)
+      const dt = await GetOrderService(params.id)
+      setDados(dt)
     }
     handleData()
     const handleOrder = async () => {
-      const reqBody = await GetIdItens(Number(itens))
-      const dt = reqBody.data
-      setData(dt)
+      if (itens && menu) {
+        const dt = await GetIdItens(itens)
+        setData(dt)
+      } else router.back()
     }
     handleOrder()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [params.id, itens, menu])
 
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault()
+    setStatus(true)
     if (itens && menu) {
       const req = await UpdateOrder(dados, params.id)
       if (req) {
         alert('Item inserido com sucesso!')
+        setStatus(false)
         router.back()
       } else {
+        setStatus(false)
         alert('Não foi possível fazer alteração!')
       }
     }
+    setStatus(false)
   }
   return (
-    <div className="flex flex-col mx-auto justify-center h-[80%] w-full items-center">
+    <div className="flex flex-col mx-auto justify-center md:h-screen min-h-96 md:mt-[-125px] w-full items-center">
       <Link
-        href="/order/list"
-        className="md:hidden flex items-center gap-1 text-sm mb-3 w-96 max-md:mt-24 max-md:w-80"
+        href="/order/menuUser"
+        className="flex items-center gap-1 text-sm mb-3 w-96 max-md:mt-4 max-md:w-80"
       >
         <ArrowLeft className="w-5 h-5" />
         Voltar
       </Link>
       <p className="text-xl mb-1 w-96 max-md:mb-0 max-md:w-80">
-        Cadastrado do pedido
+        Alteração do pedido
       </p>
       <form onSubmit={handleSubmit}>
         <div className="flex flex-col mt-3 max-md:mt-3">
@@ -175,7 +204,19 @@ export default function UpdateOrderPage({
             required
           />
         </div>
-        <BtnForm title="Cadastrar" />
+        <div className="flex flex-col mt-3 max-md:mt-3">
+          <label htmlFor="observation">
+            Observação: <span className="text-red-600">*</span>{' '}
+          </label>
+          <input
+            type="text"
+            name="observation"
+            id="observation"
+            className="w-96 h-9 border border-zinc-400 rounded-[5px] max-md:w-80 px-2"
+            required
+          />
+        </div>
+        <BtnForm pending={status} />
       </form>
     </div>
   )
